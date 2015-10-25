440186b1b0d856f2ae5a904a0e5fd56c7a381d17
Herbert Rocha <herberthb12@gmail.com>
2015-10-25 08:11:26 -0400

    Updated wrapper script
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- wrapper_script_map2check.sh
+++ wrapper_script_map2check.sh
@@ -4,8 +4,8 @@
 # Usage: ./map2check_wrapper_script.sh <file.i> <witnessfile.graphml>
 
 # Path to the Map2Check tool
-#path_to_map2check=/home/herbert/Projects/Map2Check/map2check.py
-path_to_map2check=/home/hrocha/ON_DEV/benchexec/Map2Check/map2check.py
+path_to_map2check=/home/herbert/Projects/Map2Check/map2check.py
+#path_to_map2check=/home/hrocha/ON_DEV/benchexec/Map2Check/map2check.py
 
 
 # Command line, common to all tests.
@@ -15,12 +15,15 @@ path_to_map2check=/home/hrocha/ON_DEV/benchexec/Map2Check/map2check.py
 # FAILED (if any), i.e., an execution that the tool has identified a property violation.
 map2check_options="--complete-check 2 --graphml-output --witnesspath"
 
+# Store the path to the files
+benchmark=""
+witnesspath=""
 
 # Memsafety cmdline property options
-error_label = 0
+error_label=0
 
 
-while getopts "c:mh" arg; do
+while getopts "c:mw:h" arg; do
     case $arg in
         h)
             echo "Usage: $0 [options] path_to_benchmark
@@ -32,19 +35,25 @@ Options:
             # Given the lack of variation in the property file... we don't
             # actually interpret it. Instead we have the same options to all
             # tests, except for the HeapReach, where we define the
-            # error labels to be ERROR.
-            if grep -q -E "LTL(G ! call(__VERIFIER_error()))" $OPTARG; then			
-            	error_label = 1
+            # error labels to be ERROR.            
+            error_property=`cat $OPTARG | grep -c "LTL(G ! call(__VERIFIER_error()))"`
+            if [ $error_property -gt 0 ]; then			
+            	error_label=1
+            	benchmark=
+            	echo "HERE"
             fi
             ;;
+        w)
+			# Store the path to the file to write the witness if the sourcefile
+			# contains a bug and the tool returns False (Error found)
+			witnesspath=$OPTARG
     esac
 done
 
+echo ${BASH_ARGV[0]}
 # Store the path to the file we're going to be checking.
-benchmark=$1
-# Store the path to the file to write the witness if the sourcefile
-# contains a bug and the tool returns False (Error found)
-witnesspath=$2
+benchmark=${BASH_ARGV[0]}
+
 
 if test "${benchmark}" = ""; then
     echo "No benchmark given" >&2
@@ -65,10 +74,14 @@ success=`echo ${result_check} | grep -c "VERIFICATION SUCCESSFUL"`
 # Decide which result we determined here.
 if [ $failed -gt 0 ]; then
     # Error path found
-    # Gathering the property (i.e. valid-free, valid-memtrack,valid-deref) from result
-    property=`echo ${result_check} | grep -o "FALSE.*"`
-    # Print the property
-    echo ${property}
+    if [ $error_label -eq 0 ]; then
+		# Gathering the property (i.e. valid-free, valid-memtrack,valid-deref) from result
+		property=`echo ${result_check} | grep -o "FALSE.*"`
+		# Print the property
+		echo ${property}
+	else
+		echo "FALSE"
+	fi
 #elif [ $success -gt 0 ]; then
 #    echo "TRUE"
 else

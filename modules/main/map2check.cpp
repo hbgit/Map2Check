e8ba107e70a01805e5e7ebf709515b61b3bc3744
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-01-07 00:05:26 -0400

    Renamed callBlkLoopPass calls to callPass

--- modules/main/map2check.cpp
+++ modules/main/map2check.cpp
@@ -2,10 +2,10 @@
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 using namespace boost;
-namespace po = boost::program_options; 
- 
-#include <iostream> 
-#include <string> 
+namespace po = boost::program_options;
+
+#include <iostream>
+#include <string>
 #include <algorithm>
 #include <iterator>
 #include <numeric>
@@ -13,14 +13,14 @@ namespace po = boost::program_options;
 using namespace std;
 
 #include "caller.h"
- 
-namespace 
-{ 
-  const size_t ERROR_IN_COMMAND_LINE = 1; 
-  const size_t SUCCESS = 0; 
-  const size_t ERROR_UNHANDLED_EXCEPTION = 2; 
- 
-} // namespace 
+
+namespace
+{
+  const size_t ERROR_IN_COMMAND_LINE = 1;
+  const size_t SUCCESS = 0;
+  const size_t ERROR_UNHANDLED_EXCEPTION = 2;
+
+} // namespace
 
 // A helper function to simplify the main part.
 template<class T>
@@ -38,50 +38,51 @@ void help_msg(){
 	cout << endl;
 }
 
- 
-int main(int argc, char** argv) 
-{ 
+
+int main(int argc, char** argv)
+{
 	Caller* caller;
-	
-	try 
-	{ 
-		/** Define and parse the program options 
-		 */     
-		po::options_description desc("Options"); 
+
+	try
+	{
+		/** Define and parse the program options
+		 */
+		po::options_description desc("Options");
 		desc.add_options()
-				("help,h", "\tshow help")            
+				("help,h", "\tshow help")
 				("input-file,i", po::value< vector<string> >(), "\tspecifies the files, also works only with <file.bc>")
-				("loop-pass,l", "\tanalisys code loops")            
-		; 
+				("loop-pass,l", "\tanalisys code loops")
+        ("func-pass,f", "\tconverts functions")
+		;
 
 		po::positional_options_description p;
 		p.add("input-file", -1);
 
-		po::variables_map vm; 
-		try 
-		{ 
+		po::variables_map vm;
+		try
+		{
 			po::store(po::command_line_parser(argc, argv).
-					  options(desc).positional(p).run(), vm); // can throw 
-						
-			if (vm.count("input-file") == 0) {	
+					  options(desc).positional(p).run(), vm); // can throw
+
+			if (vm.count("input-file") == 0) {
 				help_msg();
 				cout << desc;
 				return ERROR_IN_COMMAND_LINE;
 			}
 
-			/** --help option */ 
-			if ( vm.count("help")  ) 
-			{ 
+			/** --help option */
+			if ( vm.count("help")  )
+			{
 				help_msg();
 				cout << desc;
-				return SUCCESS; 
-			} 
-			
-			if (vm.count("input-file")) {	
+				return SUCCESS;
+			}
+
+			if (vm.count("input-file")) {
 				cout << "Input file: "
                  << vm["input-file"].as< vector<string> >() << "\n";
                 std::string pathfile;
-				pathfile = accumulate(begin(vm["input-file"].as< vector<string> >()), 
+				pathfile = accumulate(begin(vm["input-file"].as< vector<string> >()),
 				               end(vm["input-file"].as< vector<string> >()), pathfile);
 				string extension = boost::filesystem::extension(pathfile);
 				if(extension.compare(".bc")){
@@ -100,38 +101,38 @@ int main(int argc, char** argv)
 				 **/
 				caller  = new Caller(pathfile);
 				caller->parseIrFile();
-				//return SUCCESS; 
+				//return SUCCESS;
 			}
-			
+
 			if (vm.count("loop-pass"))
 			{
-				caller->callBlkLoopPass();
-				
+				caller->callPass();
+
 				return SUCCESS;
-			} 
-			
+			}
 
-			po::notify(vm); // throws on error, so do after help in case 
-						  // there are any problems 
-		} 
-		catch(po::error& e) 
-		{ 
-			std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
-			std::cerr << desc << std::endl; 
-			return ERROR_IN_COMMAND_LINE; 
-		} 
 
-		// application code here // 
+			po::notify(vm); // throws on error, so do after help in case
+						  // there are any problems
+		}
+		catch(po::error& e)
+		{
+			std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+			std::cerr << desc << std::endl;
+			return ERROR_IN_COMMAND_LINE;
+		}
 
-	} 
-	catch(std::exception& e) 
-	{ 
-		std::cerr << "Unhandled Exception reached the top of main: " 
-			<< e.what() << ", application will now exit" << std::endl; 
-		return ERROR_UNHANDLED_EXCEPTION; 
+		// application code here //
 
-	} 
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << "Unhandled Exception reached the top of main: "
+			<< e.what() << ", application will now exit" << std::endl;
+		return ERROR_UNHANDLED_EXCEPTION;
 
-	return SUCCESS; 
- 
-} 
+	}
+
+	return SUCCESS;
+
+}

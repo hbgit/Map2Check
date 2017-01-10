81880f8ad5d6e4892cacb4b599f9f74ed04cf0f4
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-01-09 20:58:12 -0400

    Moved map2check_pointer to another file

--- modules/src_llvm/utils/Utils.c
+++ modules/src_llvm/utils/Utils.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-void map2check_pointer(void* x, const char* name){
-  // printf("Hello %s\n", name);
-  // klee_make_symbolic(x, sizeof(*x), name);
-}
-
 int map2check_non_det_int() {
   int non_det;
   klee_make_symbolic(&non_det, sizeof(non_det), "non_det_int");

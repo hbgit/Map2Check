679405b7db081959577d2063fd09162a424ec706
Herbert Rocha <herberthb12@gmail.com>
2017-02-14 20:13:20 +0000

    Fixing bug to build
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- dependencies/llvm-build-cmake/tools/clang/tools/c-index-test/CMakeFiles/install-c-index-test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-c-index-test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-c-index-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- dependencies/llvm-build-cmake/tools/clang/tools/libclang/CMakeFiles/install-libclang.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libclang.exports"
-  "CMakeFiles/install-libclang"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-libclang.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

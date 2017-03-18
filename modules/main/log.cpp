86f9513a7f59ad2f5bdbe6e4def54a3459de9f1d
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-03-18 18:17:55 -0400

    Changes on Log

---
+++ modules/main/log.cpp
@@ -0,0 +1,43 @@
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+// #include <boost/log/sinks/sink.hpp>
+#include <string>
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+
+
+namespace Map2Check::Log {   
+
+  void Error(std::string msg) {   
+    BOOST_LOG_TRIVIAL(error) << "\033[0;31m" << msg << "\033[0m";
+  }
+
+  void Warning(std::string msg) {
+    BOOST_LOG_TRIVIAL(warning) << "\033[1;33m" << msg << "\033[0m";
+  }
+
+  void Debug(std::string msg) {
+#ifdef DEBUG
+    BOOST_LOG_TRIVIAL(debug) << "\033[0;33m" << msg << "\033[0m";
+#endif
+  }
+
+  void Info(std::string msg) {
+    BOOST_LOG_TRIVIAL(info) << "\033[0;32m" << msg << "\033[0m";
+  }
+  
+  void Fatal(std::string msg) {
+    BOOST_LOG_TRIVIAL(fatal) << "\033[0;31m" << msg << "\033[0m";
+  }
+
+
+}
--- modules/main/log.h
+++ modules/main/log.h
@@ -1,35 +1,12 @@
-#include <boost/log/trivial.hpp>
+#pragma once
 #include <string>
 
 namespace Map2Check::Log {
-  void BoostError(std::string msg) {
-    BOOST_LOG_TRIVIAL(error) << "\033[0;31m" << msg << "\033[0m";
-  }
-
-  void BoostWarning(std::string msg) {
-    BOOST_LOG_TRIVIAL(warning) << "\033[1;33m" << msg << "\033[0m";
-  }
-
-  void BoostDebug(std::string msg) {
-    BOOST_LOG_TRIVIAL(debug) << "\033[0;33m" << msg << "\033[0m";
-  }
-
-  void BoostInfo(std::string msg) {
-    BOOST_LOG_TRIVIAL(info) << "\033[0;32m" << msg << "\033[0m";
-  }
-  
-  void BoostFatal(std::string msg) {
-    BOOST_LOG_TRIVIAL(fatal) << "\033[0;31m" << msg << "\033[0m";
-  }
-
-  void TestColors() {
-    BoostWarning("This is a warning message!");
-    BoostError("This is an error message!");
-    BoostDebug("This is a debug message!");
-    BoostInfo("This is an info message!");
-    BoostFatal("This is a fatal message!");
-  }
-
-  
+  void initLog();
+  void Error(std::string msg);
+  void Warning(std::string msg);
+  void Debug(std::string msg);
+  void Info(std::string msg);  
+  void Fatal(std::string msg);  
 }
 						   

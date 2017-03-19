019901d4326313ae4e2b454318818f77f74e7984
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-03-19 08:44:05 -0400

    Updates on getting files from klee folder

--- modules/main/verifier.cpp
+++ modules/main/verifier.cpp
@@ -1,35 +1,97 @@
 #include "verifier.h"
-
+#include "log.h"
 #include <string>
 #include <vector>
-
-
+#include <sstream>
+#include <regex>
 #include <boost/filesystem.hpp>
-using namespace boost;
-#include "log.h"
-
-
 
 namespace fs = boost::filesystem;
-// using namespace Map2Check;
 
+Witness::Witness(string kleeResultFolder) {
+  genKleeFilesListFromFolder(kleeResultFolder);
+}
 
-
-std::vector<std::string> Witness::listFilesFromFolder(std::string kleeResultFolder) {
- 
-  
+void Witness::genKleeFilesListFromFolder(std::string kleeResultFolder) {
   Map2Check::Log::Debug("Obtaining files from folder: " + kleeResultFolder);
 
   // Checks if folder exists
-  if (!fs::exists(kleeResultFolder))
+  if (!fs::exists(kleeResultFolder) || !fs::is_directory(kleeResultFolder))
   {
-    Map2Check::Log::Fatal("Folder does not exist");
-    throw WitnessException::INVALID_FILE;
+    throw InvalidKleeFolderException(kleeResultFolder);   
   }
   else {
-    Map2Check::Log::Debug("Folder does exist");
+    Map2Check::Log::Debug("Folder does exist, continuing");
+    fs::directory_iterator end_iter;
 
+    std::regex ktest("(.*\).ktest");
+    std::regex error("(.*)\.assert.err");
+    for( fs::directory_iterator dir_iter(kleeResultFolder) ; dir_iter != end_iter ; ++dir_iter)
+      {
+	KleeResult result;
+	result.name = "";
+	
+	if (fs::is_regular_file(dir_iter->status()) )
+	  {	   
+	    string current_file = dir_iter->path().string();
+	    bool kleeError = false;
+	    
+	    if(std::regex_match (current_file, ktest)) {
+	      Map2Check::Log::Debug("Found ktest file: " + current_file); 
+	      std::size_t pos = current_file.find(".ktest");
+	      result.name =  current_file.substr(0,pos);
+	      result.kleeStatus = KleeStatus::OK;
+	      
+	    } else if (std::regex_match (current_file, error)) {
+	      Map2Check::Log::Debug("Found err file: " + current_file);
+	      std::size_t pos = current_file.find(".assert.err");
+	      result.name =  current_file.substr(0,pos);
+	      result.kleeStatus = KleeStatus::ERROR;
+	      
+	    }
 
-    
+	    if (result.name != "") {
+	      bool notFound = true;
+	      string key = result.name;
+	      
+	      std::vector<KleeResult>::iterator it;
+	      it = std::find_if (kleeResults.begin(),
+				 kleeResults.end(),
+				 [key] (const KleeResult& o) -> bool {
+				   return o.name == key;});
+
+	      if (it != kleeResults.end()) {
+		Map2Check::Log::Debug("Found old entry for " + it->name);
+		it->kleeStatus   = KleeStatus::ERROR;
+	      }
+	      else {
+		Map2Check::Log::Debug("Didn't found old entry for " + result.name);
+		kleeResults.push_back(result);
+		
+	      }
+	     
+	    }
+	  }
+      }
   }
+
+  if (kleeResults.size() == 0) {
+    throw InvalidKleeFolderException(kleeResultFolder);  
+  }
+}
+
+const char* WitnessException::what() const throw() {
+  std::ostringstream cnvt; 
+  cnvt.str("");
+  cnvt << runtime_error::what();
+  Map2Check::Log::Error(cnvt.str());
+  return cnvt.str().c_str();  
+}
+
+const char* InvalidKleeFolderException::what() const throw() {
+  std::ostringstream cnvt; 
+  cnvt.str("");
+  cnvt << "Folder " <<  this->folder << " does not contain Klee Results!";
+  Map2Check::Log::Error(cnvt.str());
+  return cnvt.str().c_str();  
 }
--- modules/main/verifier.h
+++ modules/main/verifier.h
@@ -4,19 +4,41 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <stdexcept>
+	
 
-/* enum class WitnessExceptions { */
-/*   INVALID_FILE */
-/* }; */
+using namespace std;
   
-enum class WitnessException {
-  INVALID_FILE
+class Witness {
+ public:   
+  Witness(string kleeResultFolder);
+ private: 
+  void genKleeFilesListFromFolder(string kleeResultFolder);
+  void convertKleeFilesToTextFiles();
+  enum class KleeStatus {
+    OK,
+    ERROR  
+  };
+  struct KleeResult {
+    KleeStatus kleeStatus;
+    string name;
+    string path;
+  };
+  vector<KleeResult> kleeResults;
+  
 };
 
+class WitnessException : public runtime_error {
+ public:
+ WitnessException(string message) : runtime_error(message) {}
+  virtual const char* what() const throw();
+};
 
-class Witness {
-  public: 
-    static void GenerateWitness(std::vector<std::string> kleeFiles);
-    static void GenerateWitness(std::string kleeResultFolder); 
-    static std::vector<std::string> listFilesFromFolder(std::string kleeResultFolder); 
-  };
+class InvalidKleeFolderException : public WitnessException {
+ public:
+ InvalidKleeFolderException(string folder) : WitnessException(""), folder(folder) {}
+  virtual const char* what() const throw();
+
+ private:
+  string folder;
+};

32861de81ebfc17e9113df1d01e2dc9cb39394b0
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-03-23 17:51:39 -0400

    Updates on Witness Implementation

--- modules/main/verifier.cpp
+++ modules/main/verifier.cpp
@@ -4,30 +4,72 @@
 #include <vector>
 #include <sstream>
 #include <regex>
+#include <fstream>
 #include <boost/filesystem.hpp>
+#include <boost/graph/adjacency_list.hpp> 
+#include <boost/graph/graphml.hpp>
+
 
 namespace fs = boost::filesystem;
 
+// TODO: make constructor private and create static method to execute
 Witness::Witness(string kleeResultFolder) {
   genKleeFilesListFromFolder(kleeResultFolder);
+  generateWitness();
+
+#ifdef DEBUG
+  boost::dynamic_properties dp;
+  boost::write_graphml(std::cout, this->graph, dp);
+#endif
 }
 
-void Witness::convertKleeFileToTextFile(KleeResult kleeResult) {
+void Witness::convertKleeFileToTextFile(KleeResult* kleeResult) {
   Map2Check::Log::Debug("Converting ktest to test file");
   
   std::ostringstream command; 
   command.str("");  
   command << Map2Check::Tools::ktestBinary << " --write-ints ";
-  command << kleeResult.name << ".ktest";
-  command << " >> " << kleeResult.name << ".test";
+  command << kleeResult->name << ".ktest";
+  command << " >> " << kleeResult->name << ".test";
 
   Map2Check::Log::Debug(command.str());
   system(command.str().c_str());
+
+  std::regex reData("object .*: data: (.*)");
+  // TODO: Use regex to determine number of objects
+  //       and use fixed array list instead of vector
+  
+  // std::regex reNumObjects("num objects: (.*)");
+  std::smatch match;
+  std::string result;
+  
+  std::string line;
+  std::ostringstream filename;
+  filename.str("");
+  filename  << kleeResult->name << ".test";
+
+  std::ifstream testFile;
+  testFile.open(filename.str().c_str());
+
+  if(testFile.is_open()) {
+    while ( std::getline (testFile,line) )
+    {
+      if (std::regex_search(line, match, reData) && match.size() > 1) {
+	Map2Check::Log::Debug("Adding " + match.str(1) + " to " + kleeResult->name);
+	int result = std::stoi(match.str(1));
+	kleeResult->states.push_back(result);	
+      }       
+    }
+    testFile.close();
+    
+  }
+  else {
+    throw ErrorOpeningFileException();
+  }  
 }  
 
 void Witness::genKleeFilesListFromFolder(std::string kleeResultFolder) {
   Map2Check::Log::Debug("Obtaining files from folder: " + kleeResultFolder);
-
 
   // Checks if folder exists
   if (!fs::exists(kleeResultFolder) || !fs::is_directory(kleeResultFolder))
@@ -97,11 +139,89 @@ void Witness::genKleeFilesListFromFolder(std::string kleeResultFolder) {
     throw InvalidKtestBinException();
   }
   
-  std::for_each(kleeResults.begin(),
-		kleeResults.end(),
-		convertKleeFileToTextFile);
+  for(int i; i < this->kleeResults.size(); i++) {
+    convertKleeFileToTextFile(&this->kleeResults[i]);
+    
+  }
   
 }
+
+void Witness::generateWitness() {
+   Map2Check::Log::Debug("Started graph creation");
+
+   // Creates initial state for graph   
+   vertex_t initialState = boost::add_vertex(this->graph);
+   this->graph[initialState].status = StateStatus::NON_ACCEPTING;
+   
+   for(auto i = this->kleeResults.begin();
+       i != this->kleeResults.end(); i++) {
+     KleeResult kleeResult = *i;
+     Map2Check::Log::Debug("Got test: " + kleeResult.name);
+     vertex_t currentState = initialState;
+     
+     for(int j = 0; j < kleeResult.states.size(); j++) {
+       Witness::Graph::edge_iterator ei, eiEnd;
+       std::vector<edge_t> adjacentEdges;
+       
+       for (boost::tie(ei, eiEnd) = boost::edges(this->graph);
+	    ei != eiEnd; ++ei) {
+	 vertex_t source = boost::source(*ei, this->graph);
+	 if(source == currentState) {	  
+	   adjacentEdges.push_back(*ei);
+	   Map2Check::Log::Debug("Found transition");
+	 }	 
+       }
+
+       int value = kleeResult.states[j];
+       bool foundEdge = false;
+
+       for (auto adj = adjacentEdges.begin();
+	    adj != adjacentEdges.end(); adj++) {
+	 if(this->graph[*adj].value == value) {
+	   Map2Check::Log::Debug("Found edge for transition with value: " + std::to_string(value));
+	   foundEdge = true;
+	   currentState  = boost::target(*adj, this->graph);
+	   break;
+	 }
+       }
+
+       if (!foundEdge) {
+	 Map2Check::Log::Debug("Creating new edge with value: " +  std::to_string(value));
+	 vertex_t source = currentState;
+	 currentState = boost::add_vertex(this->graph);
+	 this->graph[currentState].status = StateStatus::NON_ACCEPTING;
+	 this->generateEdge(source, currentState, value);
+       }
+     }        
+
+     this->changeStateStatus(currentState, kleeResult.kleeStatus);  
+   }
+   
+}
+
+void Witness::changeStateStatus(vertex_t state, KleeStatus status) {
+  StateStatus stateStatus = status == KleeStatus::OK ?
+    StateStatus::SINK : StateStatus::ACCEPTING;
+  if(this->graph[state].status == StateStatus::NON_ACCEPTING)
+    {
+      Map2Check::Log::Debug("Changing state status");
+      this->graph[state].status = stateStatus;
+    }
+  else if ( this->graph[state].status != stateStatus) {
+    throw AmbigousKleeResult();
+  }	  
+}
+
+void Witness::generateEdge(vertex_t origin,
+			   vertex_t destiny,
+			   int weight) {
+
+  edge_t e; bool b;   
+  boost::tie(e,b) = boost::add_edge(origin, destiny, this->graph);
+
+  this->graph[e].value = weight;  
+}
+
 
 const char* WitnessException::what() const throw() {
   std::ostringstream cnvt; 
@@ -123,6 +243,22 @@ const char* InvalidKtestBinException::what() const throw() {
   std::ostringstream cnvt; 
   cnvt.str("");
   cnvt << "Could not find binary " <<  Map2Check::Tools::ktestBinary;
+  Map2Check::Log::Error(cnvt.str());
+  return cnvt.str().c_str();  
+}
+
+const char* ErrorOpeningFileException::what() const throw() {
+  std::ostringstream cnvt; 
+  cnvt.str("");
+  cnvt << "Could not open file in reading mode";
+  Map2Check::Log::Error(cnvt.str());
+  return cnvt.str().c_str();  
+}
+
+const char* AmbigousKleeResult::what() const throw() {
+  std::ostringstream cnvt; 
+  cnvt.str("");
+  cnvt << "Klee determined state to be Sink and Accepting";
   Map2Check::Log::Error(cnvt.str());
   return cnvt.str().c_str();  
 }
--- modules/main/verifier.h
+++ modules/main/verifier.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <boost/graph/adjacency_list.hpp> 
 	
 using namespace std;
 
@@ -14,20 +15,48 @@ namespace Map2Check::Tools {
   
 class Witness {
  public:   
-  Witness(string kleeResultFolder);
+  Witness(string kleeResultFolder);  
+
   enum class KleeStatus {
     OK,
     ERROR  
   };
+  
   struct KleeResult {
     KleeStatus kleeStatus;
+    vector<int> states;
     string name;
   };  
  private:  
   void genKleeFilesListFromFolder(string kleeResultFolder);  
-  static void convertKleeFileToTextFile(KleeResult kleeResult); 
+  static void convertKleeFileToTextFile(KleeResult* kleeResult);
+  void generateWitness();
   vector<KleeResult> kleeResults;
+
+  enum class StateStatus {
+    SINK,         // Final State with no ERROR
+    ACCEPTING,    // Final State with ERROR
+    NON_ACCEPTING // Normal State
+  };
   
+  struct State {
+    int id;
+    StateStatus status;
+  };
+
+   struct Edge {
+    int value;
+  };
+  
+   typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, State, Edge > Graph;
+   /* typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, State > Graph; */
+   typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
+   typedef boost::graph_traits<Graph>::edge_descriptor edge_t;
+
+   Graph graph;
+
+   void generateEdge(vertex_t origin, vertex_t destiny, int weight);
+   void changeStateStatus(vertex_t state, KleeStatus status);
 };
 
 class WitnessException : public runtime_error {
@@ -49,4 +78,16 @@ class InvalidKtestBinException : public WitnessException {
    public:
  InvalidKtestBinException() : WitnessException("") {}
   virtual const char* what() const throw();  
+};
+
+class ErrorOpeningFileException : public WitnessException {
+ public:
+ ErrorOpeningFileException() : WitnessException("") {}
+  virtual const char* what() const throw();    
+};
+
+class AmbigousKleeResult : public WitnessException {
+ public:
+ AmbigousKleeResult() : WitnessException("") {}
+  virtual const char* what() const throw();    
 };

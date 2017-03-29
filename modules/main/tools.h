7fe61c26d89875219fcf42b288258d52bc8c3ff6
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-03-29 11:39:17 -0400

    Added header containing tools namespace

---
+++ modules/main/tools.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+namespace Map2Check::Tools {
+  const string ktestBinary("./bin/ktest-tool");
+  const string clangBinary("./bin/clang");
+  const string clangIncludeFolder("./include/clang");
+}
+

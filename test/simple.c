c4bfed1e51fd66072aead60acf7eed7c9092565c
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-03-29 14:34:33 -0400

    Added simple test

---
+++ test/simple.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int* A = malloc(sizeof(int));
+
+  int x = __VERIFIER_nondet_int();
+
+  if(x) {
+    x = __VERIFIER_nondet_int();
+    if (x) {
+      free(A);
+    }
+  }
+
+  free(A);
+}

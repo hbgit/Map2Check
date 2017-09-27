33faa7382ce424fa84d33e7638b02a1035f43de5
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-09-27 15:25:00 -0400

    Added pass for target functions

--- modules/backend/pass/TargetPass.h
+++ modules/backend/pass/TargetPass.h
@@ -1,11 +1,40 @@
-#ifndef TARGETPASS_H
-#define TARGETPASS_H
+#pragma once
 
+#include <llvm/Pass.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Metadata.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/Support/raw_ostream.h>
 
-class TargetPass
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#include "DebugInfo.hpp"
+
+using namespace llvm;
+
+struct TargetPass : public FunctionPass
 {
-public:
-    TargetPass();
-};
+    static char ID;
+    TargetPass() : FunctionPass(ID) {}
+    TargetPass(std::string FunctionName) : FunctionPass(ID) {
+        targetFunctionName = FunctionName;
+    }
+    virtual bool runOnFunction(Function &F);
 
-#endif // TARGETPASS_H
+protected:
+    Value* getFunctionNameValue() { return this->functionName; }
+    void runOnCallInstruction(CallInst* callInst, LLVMContext* Ctx);
+    void instrumentErrorInstruction(CallInst* callInst, LLVMContext* Ctx);
+
+private:
+    BasicBlock::iterator currentInstruction;
+    Constant *targetFunctionMap2Check = NULL;
+    Value* functionName = NULL;
+    std::string targetFunctionName;
+};

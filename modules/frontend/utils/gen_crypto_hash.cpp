05f36d85a752609da8409e047da0154ea332a8a4
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2017-11-29 22:04:31 -0200

    Fixed warnings

--- modules/frontend/utils/gen_crypto_hash.cpp
+++ modules/frontend/utils/gen_crypto_hash.cpp
@@ -9,71 +9,46 @@
 #include <boost/format.hpp>
 
 
-void GenHash::setOutputSha1HashFile( std::string keysha1 )
-{
-	this->key_sha1_hash_file = keysha1;
+void GenHash::setOutputSha1HashFile(std::string keysha1) {
+  this->key_sha1_hash_file = keysha1;
 }
 
-std::string GenHash::getOutputSha1HashFile()
-{
-	return this->key_sha1_hash_file;
+std::string GenHash::getOutputSha1HashFile() {
+  return this->key_sha1_hash_file;
 }
 
-
-void GenHash::setFilePath( std::string filepath )
-{
-	this->filepath = filepath;
+void GenHash::setFilePath(std::string filepath) {
+  this->filepath = filepath;
 }
 
-std::string GenHash::getFilePath()
-{
-	return this->filepath;
+std::string GenHash::getFilePath() {
+  return this->filepath;
 }
 
 
-int GenHash::generate_sha1_hash_for_file()
-{    
-    unsigned int hash[5];    
-    std::stringstream ss;
-    
-	std::ifstream file(this->filepath.c_str(), std::ios::binary | std::ios::ate);
-	std::streamsize size = file.tellg();
-	file.seekg(0, std::ios::beg);
+int GenHash::generate_sha1_hash_for_file() {    
+  unsigned int hash[5];
+  std::stringstream ss;
+  std::ifstream file(this->filepath.c_str(), std::ios::binary | std::ios::ate);
+  std::streamsize size = file.tellg();
+  file.seekg(0, std::ios::beg);
 
-	std::vector<char> buffer(size);
-	if (file.read(buffer.data(), size))
-	{
-		/* worked! */
-		//cout << buffer.data();		
-		
-		this->boost_sha1.process_bytes(buffer.data(), size);    
-		this->boost_sha1.get_digest(hash);
-	}else
-	{
-		return -1; //not work
-	}
-	    
-    
-    for(std::size_t i=0; i<sizeof(hash)/sizeof(hash[0]); ++i) {        
-		ss << std::hex << std::setfill('0') << std::setw(sizeof(int)*2) 
-			<< hash[i];        
-    }    
-	    
-    this->key_sha1_hash_file = ss.str();
-    
+  std::vector<char> buffer(size);
+  if (file.read(buffer.data(), size)) {
+    this->boost_sha1.process_bytes(buffer.data(), size);
+    this->boost_sha1.get_digest(hash);
+  } else {
+    return -1;
+  }
+  for (std::size_t i=0; i < sizeof(hash)/sizeof(hash[0]); ++i) {
+    ss << std::hex << std::setfill('0') << std::setw(sizeof(int)*2)
+       << hash[i];
+  }
+
+  this->key_sha1_hash_file = ss.str();
+  return 0;
 }
 
-void GenHash::print_sha1_hash_for_file()
-{
-	std::cout << this->key_sha1_hash_file << std::endl;
+void GenHash::print_sha1_hash_for_file() {
+  std::cout << this->key_sha1_hash_file << std::endl;
 }
-
-/*
-main(){
-
-  GenHash T;
-  T.setFilePath("gen_crypto_hash.hpp");
-  T.getFilePath();
-  T.generate_sha1_hash_for_file();
-  T.print_sha1_hash_for_file();
-}**/

cf47c4eeba346830a9fe25441061cdcea6486181
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-05-05 17:52:03 -0300

    Converted project to a empty project

--- modules/frontend/caller.cpp
+++ modules/frontend/caller.cpp
@@ -29,13 +29,13 @@
 #include <boost/filesystem.hpp>
 
 // Project Libs
-#include "../backend/pass/MemoryTrackPass.h"
-#include "../backend/pass/GenerateAutomataTruePass.h"
-#include "../backend/pass/TrackBasicBlockPass.hpp"
-#include "../backend/pass/NonDetPass.hpp"
-#include "../backend/pass/Map2CheckLibrary.hpp"
-#include "../backend/pass/TargetPass.h"
-#include "../backend/pass/OverflowPass.h"
+// #include "../backend/pass/MemoryTrackPass.h"
+// #include "../backend/pass/GenerateAutomataTruePass.h"
+// #include "../backend/pass/TrackBasicBlockPass.hpp"
+// #include "../backend/pass/NonDetPass.hpp"
+// #include "../backend/pass/Map2CheckLibrary.hpp"
+// #include "../backend/pass/TargetPass.h"
+// #include "../backend/pass/OverflowPass.h"
 #include "utils/log.hpp"
 #include "utils/tools.hpp"
 
@@ -67,250 +67,49 @@ Caller::Caller(std::string bcprogram_path) {
 }
 
 std::string Caller::preOptimizationFlags() {
-  std::ostringstream flags;
-  flags.str("");
-  flags << "-O0";
-  return flags.str();
+
 }
 
 std::string Caller::postOptimizationFlags() {
-  std::ostringstream flags;
-  flags.str("");
-  flags << "-O3";
-  return flags.str();
+  
 }
 
 void Caller::cleanGarbage() {
-  std::ostringstream removeCommand;
-  removeCommand.str("");
-  removeCommand << "rm -rf"
-              << " klee-*"
-              << " *.log"
-              << " list-*"
-              << " *.csv"
-              << " map2check_property"
-              << " automata_list_log.st"
-              << " track_bb_log.st"
-              << " map2check_property_klee_unknown"
-              << " map2check_property_klee_deref"
-              << " map2check_property_klee_memtrack"
-              << " map2check_property_overflow"
-              << " map2check_property_klee_free"
-              << " preprocessed.c"
-              << " optimized.bc"
-              << " output.bc"
-              << " witnessInfo";
-
-  int result = system(removeCommand.str().c_str());
-  if (result == -1) {
-    throw CallerException("Could not remove files");
-  }
+  
 }
 
 void Caller::parseIrFile() {
-  Map2Check::Log::Debug("Parsing file " + this->pathprogram);
-  StringRef filename = this->pathprogram;
 
-  SMDiagnostic SM;
-  LLVMContext & Context = getGlobalContext();
-  M = parseIRFile(filename, SM, Context);
-
-  if (!SM.getMessage().empty()) {
-    check("Problem reading input bitcode/IR: " + SM.getMessage().str());
-  } else {
-    std::cout << "Successfully read bitcode/IR" << std::endl;
-  }
 }
 
 int Caller::callPass(Map2CheckMode mode, bool sv_comp) {
-  llvm::legacy::PassManager AnalysisPasses;
-  // Pass to generate_automata_true
-  Map2Check::Log::Debug("Applying GenerateAutomataTruePass\n");
-  AnalysisPasses.add(new GenerateAutomataTruePass(this->cprogram_fullpath));
-
-  /* TrackBasicBlockPass is here to avoid lost
-     position after code instrumentation */
-  Map2Check::Log::Debug("Applying TrackBasicBlockPass\n");
-  AnalysisPasses.add(new TrackBasicBlockPass(this->cprogram_fullpath));
-
-  Map2Check::Log::Debug("Applying NonDetPass\n");
-  AnalysisPasses.add(new NonDetPass());
-
-  switch (mode) {
-    case (Map2CheckMode::MEMTRACK_MODE):
-      Map2Check::Log::Debug("Applying MemoryTrackPass\n");
-      AnalysisPasses.add(new MemoryTrackPass(sv_comp));
-      break;
-    case (Map2CheckMode::OVERFLOW_MODE):
-      Map2Check::Log::Debug("Applying OverflowPass\n");
-      {
-        std::vector<int> lines = this->processClangOutput();
-        AnalysisPasses.add(new OverflowPass(lines));
-      }
-      break;
-    default:
-      throw CallerException("INVALID MODE FOR THIS FUNCTION PROTOTYPE");
-  }
-
-  Map2Check::Log::Debug("Applying Map2CheckLibrary\n");
-  AnalysisPasses.add(new Map2CheckLibrary(sv_comp));
-  AnalysisPasses.run(*M);
-  Map2Check::Log::Debug("Finished instrumentation\n");
-
   return 1;
 }
 
 
 int Caller::callPass(Map2CheckMode mode, std::string target_function,
                      bool sv_comp) {
-  llvm::legacy::PassManager AnalysisPasses;
-  // Pass to generate_automata_true
-  Map2Check::Log::Debug("Applying GenerateAutomataTruePass\n");
-  AnalysisPasses.add(new GenerateAutomataTruePass(this->cprogram_fullpath));
-
-  // TODO(hbrocha) Improve time verification
-  Map2Check::Log::Debug("Applying TrackBasicBlockPass\n");
-  AnalysisPasses.add(new TrackBasicBlockPass(this->cprogram_fullpath));
-
-  Map2Check::Log::Debug("Applying NonDetPass\n");
-  AnalysisPasses.add(new NonDetPass());
-  switch (mode) {
-    case (Map2CheckMode::REACHABILITY_MODE):
-      Map2Check::Log::Debug
-          ("Starting target pass with function " + target_function);
-      AnalysisPasses.add(new TargetPass(target_function));
-      break;
-    default:
-      throw CallerException("INVALID MODE FOR THIS FUNCTION PROTOTYPE");
-  }
-
-  Map2Check::Log::Debug("Applying Map2CheckLibrary\n");
-  AnalysisPasses.add(new Map2CheckLibrary(sv_comp));
-  AnalysisPasses.run(*M);
-  Map2Check::Log::Debug("Finished instrumentation\n");
-  return 1;
+  
 }
 
 void Caller::generateProcessedByteCodeFile() {
-  const char *Filename = "output.bc";
-  errs() << "";
-  std::error_code EC;
-  llvm::raw_fd_ostream file_descriptor(Filename, EC, llvm::sys::fs::F_None);
-  WriteBitcodeToFile(&(*M), file_descriptor);
-  file_descriptor.flush();
+
 }
 
 void Caller::linkLLVM() {
-  /* Link functions called after executing the passes */
-  // TODO(rafa.sa.xp@gmail.com) Only link against used libraries
 
-  std::ostringstream linkCommand;
-  linkCommand.str("");
-  linkCommand << Map2Check::Tools::llvmLinkBinary;
-  linkCommand
-      << " output.bc"
-      << " ${MAP2CHECK_PATH}/lib/Map2CheckFunctions.bc"
-      << " ${MAP2CHECK_PATH}/lib/AllocationLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/HeapLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/TrackBBLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/BTree.bc"
-      << " ${MAP2CHECK_PATH}/lib/Container.bc"
-      << " ${MAP2CHECK_PATH}/lib/KleeLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/ListLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/PropertyGenerator.bc"
-      << " ${MAP2CHECK_PATH}/lib/BinaryOperation.bc "
-      << "  > result.bc";
-
-  if (system(linkCommand.str().c_str()) != 0) {
-      throw CallerException("Could not link against map2check files");
-  }
-  // TODO(rafa.sa.xp@gmail.com) Check how optimization can generate errors
-  std::ostringstream optimizeCommand;
-  optimizeCommand.str("");
-  optimizeCommand << Map2Check::Tools::optBinary;
-  optimizeCommand << " " << Caller::postOptimizationFlags()
-           << " result.bc > optimized.bc";
-  if (system(optimizeCommand.str().c_str()) != 0) {
-    throw CallerException("Could not execute optimization command");
-  }
 }
 
 void Caller::callKlee() {
-  std::ostringstream kleeCommand;
-  kleeCommand.str("");
-  kleeCommand << Map2Check::Tools::kleeBinary;
-  kleeCommand << " -suppress-external-warnings"
-          << " --allow-external-sym-calls"
-          << " -exit-on-error-type=Assert"
-          << " --optimize optimized.bc"
-          << "  > ExecutionOutput.log";
-  if (system(kleeCommand.str().c_str()) != 0) {
-    Map2Check::Log::Debug("Klee finished with erorr");
-  } else {
-    Map2Check::Log::Debug("Klee finished");
-  }
+
 }
 
 std::vector<int> Caller::processClangOutput() {
-  const char* path_name = "clang.out";
 
-  std::vector<int> result;
-
-  ifstream in(path_name);
-  if (!in.is_open()) {
-    Map2Check::Log::Debug("Clang did not generate warning or errors");
-    return result;
-  }
-
-  Map2Check::Log::Debug("Clang generate warning or errors");
-
-  regex overflowWarning(".*:([[:digit:]]+):[[:digit:]]+:.*Winteger-overflow.*");
-  string line;
-  smatch match;
-  while (getline(in, line)) {
-    if (std::regex_search(line, match, overflowWarning) && match.size() > 1) {
-      Map2Check::Log::Info("Found warning at line " + match[1].str());
-      int lineNumber = std::stoi(match[1].str());
-      result.push_back(lineNumber);
-    }
-  }
-  if (system("rm clang.out") != 0) {
-    throw CallerException("Failed to remove clang.out");
-  }
-  return result;
 }
 
 string Caller::compileCFile(std::string cprogram_path) {
-  Map2Check::Log::Info("Compiling " + cprogram_path);
 
-  std::ostringstream commandRemoveExternMalloc;
-  commandRemoveExternMalloc.str("");
-  commandRemoveExternMalloc << "cat " << cprogram_path << " | ";
-  commandRemoveExternMalloc << "sed -e 's/.*extern.*malloc.*//g' "
-                            << "  -e 's/.*void \\*malloc(size_t size).*//g' "
-                            <<" > preprocessed.c";
-
-  int resultRemove = system(commandRemoveExternMalloc.str().c_str());
-  if (resultRemove == -1) {
-    throw ErrorCompilingCProgramException();
-  }
-  std::ostringstream command;
-  command.str("");
-  command << Map2Check::Tools::clangBinary << " -I"
-          << Map2Check::Tools::clangIncludeFolder
-          << " -Wno-everything "
-          << " -Winteger-overflow "
-          << " -c -emit-llvm -g"
-          << " " << Caller::preOptimizationFlags()
-          << " -o compiled.bc "
-          << "preprocessed.c"
-          << " > clang.out 2>&1";
-
-  int result = system(command.str().c_str());
-  if (result != 0) {
-    throw ErrorCompilingCProgramException();
-  }
-  return ("compiled.bc");
 }
 
 const char* CallerException::what() const throw() {
--- modules/frontend/map2check.cpp
+++ modules/frontend/map2check.cpp
@@ -1,4 +1,3 @@
-
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 //using namespace boost;
@@ -14,35 +13,23 @@ namespace po = boost::program_options;
 #include <memory>
 using namespace std;
 
-#include "caller.hpp"
-// #include "verifier.h"
-#include "utils/log.hpp"
-#include "exceptions.hpp"
-#include "utils/tools.hpp"
-#include "counter_example/counter_example.hpp"
-#include "witness/witness_include.hpp"
-//#include "witness/witness.hpp"
-#include "utils/gen_crypto_hash.hpp"
 
 #define Map2CheckVersion "Map2Check 7.1 : Wed Nov 22 22:30:11 -04 2017"
 
-namespace
-{
+namespace {
     const size_t SUCCESS = 0;
     const size_t ERROR_IN_COMMAND_LINE = 1;
     const size_t ERROR_UNHANDLED_EXCEPTION = 2;
 
-} // namespace
-
+}  // namespace
 // A helper function to simplify the main part.
     template<class T>
-ostream& operator<<(ostream& os, const std::vector<T>& v)
-{
+ostream& operator<<(ostream& os, const std::vector<T>& v) {
     copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
     return os;
 }
 
-void help_msg(){
+void help_msg() {
     cout << endl;
     cout << "> > > \t  "<< Map2CheckVersion << " \t < < <" << endl;
     cout << endl;
@@ -58,337 +45,28 @@ int MIN(int a, int b) {
 }
 
 
-int main(int argc, char** argv)
-{
-    std::unique_ptr<Caller> caller;
-    namespace fs = boost::filesystem;
+int main(int argc, char** argv) {
+  try {
+    /** Define and parse the program options
+     */
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help,h", "\tshow help")
+        ("version,v", "\tprints map2check version")
+        ("input-file,i",
+         po::value< std::vector<string> >(),
+         "\tspecifies the files, also works only with <file.bc>");
 
+    po::positional_options_description p;
+    p.add("input-file", -1);
 
-#ifndef __unix__
-    throw Map2Check::Exceptions::Map2CheckException("Map2Check only supports unix environments");
-#endif
+    po::variables_map vm;
+  }
+  catch(std::exception& e) {
+    std::cerr << "Unhandled Exception reached the top of main: "
+              << e.what() << ", application will now exit" << std::endl;
+    return ERROR_UNHANDLED_EXCEPTION;
+  }
 
-    char szTmp[32];
-    int len = 500;
-    char pBuf[len];
-    sprintf(szTmp, "/proc/%d/exe", getpid());
-    int bytes = MIN(readlink(szTmp, pBuf, len), len - 1);
-    std::string map2check_bin(argv[0]);
-    int deleteSpace = 0;
-    if(map2check_bin.size() > 9) {
-        deleteSpace = 10;
-    }
-    else {
-        deleteSpace = 9;
-    }
-    if(bytes >= 0) {
-        pBuf[bytes - deleteSpace] = '\0';
-    }
-    else {
-        // throw error
-    }
-
-    std::string map2check_env_var("MAP2CHECK_PATH=");
-    map2check_env_var += pBuf; 
-
-
-
-    putenv((char*) map2check_env_var.c_str() );
-
-    // TODO: put those vars in a anonimous namespace
-    // fs::path p("${MAP2CHECK_PATH}/lib/klee/runtime");
-    // fs::path klee_lib_path = fs::complete(p); // complete == absolute
-    std::string klee_env_var("KLEE_RUNTIME_LIBRARY_PATH=");
-    klee_env_var += pBuf;
-    klee_env_var += "/lib/klee/runtime";
-
-
-    putenv((char*) klee_env_var.c_str() );
-    //std::system("echo $KLEE_RUNTIME_LIBRARY_PATH");
-    //caller->cleanGarbage();
-
-
-    try
-    {
-        /** Define and parse the program options
-        */
-        po::options_description desc("Options");
-        desc.add_options()
-            ("help,h", "\tshow help")
-            ("version,v", "\tprints map2check version")
-            ("input-file,i", po::value< std::vector<string> >(), "\tspecifies the files, also works only with <file.bc>")
-            ("target-function,f", po::value< string >(), "\tchecks if function can be executed")
-            ("check-overflow,o", "\tchecks if an integer overflow can occur")
-            ("expected-result,e", po::value< string >(), "\tspecifies what output should be, used for tests")
-            ("print-list-log,p", "\tprints list log during counter example")
-            ("generate-witness,w", "\tgenerate witness file")
-            ("generate-instrumentated-only", "\tgenerate instrumentated file and stops")
-            ("debug-info,d", "\tprints debug info")
-            ("assume-malloc-true,m", "\tassumes that all mallocs will not fail")
-            ("html-output", "\tgenerates html file with results")
-
-            ;
-
-        po::positional_options_description p;
-        p.add("input-file", -1);
-
-        po::variables_map vm;
-        try
-        {
-            po::store(po::command_line_parser(argc, argv).
-                    options(desc).positional(p).run(), vm); // can throw
-
-            if ( vm.count("version")  )
-            {
-                cout << Map2CheckVersion << "\n";
-                return SUCCESS;
-            }
-            if ( vm.count("help") == 0 && vm.count("input-file") == 0) {
-                help_msg();
-                cout << desc;
-                return ERROR_IN_COMMAND_LINE;
-            }
-
-            /** --help option */
-            if ( vm.count("help")  )
-            {
-                help_msg();
-                cout << desc;
-                return SUCCESS;
-            }
-
-            bool is_html_output = false;
-            if ( vm.count("html-output")  )
-            {
-              is_html_output = true;
-            }
-
-            if ( !vm.count("debug-info")  ) {
-                Map2Check::Log::initLog();
-
-            }
-            if (vm.count("input-file")) {
-                // TODO: Refactor for better reading
-                // cout << "Input file: "<< vm["input-file"].as< std::vector<string> >() << "\n";
-                std::string pathfile;
-                pathfile = accumulate(boost::begin(vm["input-file"]
-                            .as< std::vector<string> >
-                            ()),
-                        boost::end(vm["input-file"]
-                            .as< std::vector<string> >
-                            ()), pathfile);
-
-                string extension = boost::filesystem::extension(pathfile);
-                if(extension.compare(".bc") && extension.compare(".c") && extension.compare(".i")){
-                    help_msg();
-                    cout << desc;
-                    return ERROR_IN_COMMAND_LINE;
-                }
-                /**
-                 * Start Map2Check algorithm -- See SEFM'15 paper
-                 * (1) Tracking variables
-                 * (2) Generate values to nondet_* functions
-                 * (3) Generate claims adopting ESBMC
-                 * (4) Translating ESBMC claims into assertions
-                 * (5) Added ESBMC claims in the analyzed code
-                 * (6) Generating C code to execute the assertions
-                 **/
-                Map2Check::Log::Info("Started Map2Check");
-                if(extension.compare(".bc")) {
-                    caller = make_unique<Caller>(Caller::compileCFile(pathfile));
-                } else {
-                    caller  =  make_unique<Caller>(pathfile);
-                }
-
-                caller->cprogram_fullpath = pathfile;
-
-                caller->parseIrFile();
-                bool sv_comp = false;
-                if (vm.count("assume-malloc-true")) {
-                    sv_comp = true;
-                }
-
-                /* Checks for which mode map2check will operate, can throw error if user calls multiple modes at once */
-                // BEGIN CHECKING OPERATION MODE
-
-                Map2CheckMode map2checkMode = Map2CheckMode::MEMTRACK_MODE;
-
-                if (vm.count("target-function")) {
-                    if(map2checkMode != Map2CheckMode::MEMTRACK_MODE) {
-                        throw Map2Check::Exceptions::Map2CheckException("Map2Check does not support multiple analysis at the same time");
-                    }
-                    map2checkMode = Map2CheckMode::REACHABILITY_MODE;
-                }
-
-                if (vm.count("check-overflow")) {
-                    if(map2checkMode != Map2CheckMode::MEMTRACK_MODE) {
-                        throw Map2Check::Exceptions::Map2CheckException("Map2Check does not support multiple analysis at the same time");
-                    }
-                    map2checkMode = Map2CheckMode::OVERFLOW_MODE;
-                }
-
-                switch(map2checkMode) {
-                    case (Map2CheckMode::REACHABILITY_MODE):  {
-                                                                  string function = vm["target-function"].as< string >();
-                                                                  caller->callPass(map2checkMode, function, sv_comp);
-                                                                  break;
-                                                              }
-                    default: {
-                                 caller->callPass(map2checkMode, sv_comp);
-                             }
-
-
-                }
-
-                // END CHECKING OPERATION MODE		
-                caller->generateProcessedByteCodeFile();
-
-                if ( vm.count("generate-instrumentated-only")  )
-                {
-                    return SUCCESS;
-                }
-                caller->linkLLVM();
-
-                //return SUCCESS;//STOP
-                Map2Check::Log::Info("Started klee execution");
-                caller->callKlee();
-
-                //Check verification result [TRUE or FALSE]
-                namespace tools = Map2Check::Tools;
-                std::unique_ptr<Map2Check::CounterExample> counterExample = make_unique<Map2Check::CounterExample>(std::string(pathfile));
-                tools::PropertyViolated propertyViolated = counterExample->getProperty();
-                if(propertyViolated == tools::PropertyViolated::NONE){ // This means that result was TRUE	   
-                    Map2Check::Log::Info(" \n");
-                    Map2Check::Log::Info("VERIFICATION SUCCEDED \n");
-
-                    if(is_html_output) {
-                      std::ostringstream command;
-                      command.str("");
-                      command << "echo '<pre>";
-                      command << "<strong>VERIFICATION SUCCEDED</strong>";
-                      command << " </pre>' > out.html";
-                      if( system(command.str().c_str()) ) {
-                        Map2Check::Log::Error("Failed to Generate HTML \n");
-                      }
-                    }
-
-                }else if(propertyViolated == tools::PropertyViolated::UNKNOWN)
-                {
-                    Map2Check::Log::Info("Unable to prove or falsify the program. \n");
-                    Map2Check::Log::Info("VERIFICATION UNKNOWN \n");
-
-                    if(is_html_output) {
-                      std::ostringstream command;
-                      command.str("");
-                      command << "echo '<pre>";
-                      command << "<strong>Unable to prove or falsify the program.</strong>";
-                      command << " </pre>' > out.html";
-                      if( system(command.str().c_str()) ) {
-                        Map2Check::Log::Error("Failed to Generate HTML \n");
-                      }
-                    }
-                }else{
-
-                    Map2Check::Log::Info("Started counter example generation");	    
-                    counterExample->printCounterExample();
-
-                     if(is_html_output) {
-                      std::ostringstream command;
-                      command.str("");
-                      command << "echo '<pre> ";
-                      command << counterExample->getHTML();
-                      command << " </pre>' > out.html";
-                      if( system(command.str().c_str()) ) {
-                        Map2Check::Log::Error("Failed to Generate HTML \n");
-                      }
-                    }
-                }
-
-
-                if(vm.count("generate-witness")) {
-                    //Generating hash key to the witness
-                    GenHash genhashkey;
-                    genhashkey.setFilePath(pathfile);
-                    genhashkey.generate_sha1_hash_for_file();
-
-                    if ((propertyViolated != tools::PropertyViolated::NONE) && (propertyViolated != tools::PropertyViolated::UNKNOWN)){
-                        if (vm.count("target-function")) {
-                            string function = vm["target-function"].as< string >();
-                            Map2Check::SVCompWitness svcomp(pathfile, genhashkey.getOutputSha1HashFile(), function);
-                            svcomp.Testify();
-                        }
-                        else {
-                            Map2Check::SVCompWitness svcomp(pathfile, genhashkey.getOutputSha1HashFile());
-                            svcomp.Testify();
-                        }
-                    }else if (propertyViolated == tools::PropertyViolated::NONE)
-                    {
-                        //Correctness witness
-                        Map2Check::Log::Debug("Starting Correctness Automata Generation");
-                        if (vm.count("target-function")) {
-                            string function = vm["target-function"].as< string >();
-                            Map2Check::SVCompWitness svcomp(pathfile, genhashkey.getOutputSha1HashFile(), function, "target-function");                    
-                            svcomp.Testify();
-                        }
-                        else {                    
-                            if(map2checkMode == Map2CheckMode::OVERFLOW_MODE)
-                            {						
-                                Map2Check::SVCompWitness svcomp(pathfile, genhashkey.getOutputSha1HashFile(),"","overflow");
-                                svcomp.Testify();
-                            }else{						
-                                Map2Check::SVCompWitness svcomp(pathfile, genhashkey.getOutputSha1HashFile(),"","safetyMemory");
-                                svcomp.Testify();
-                            }
-
-
-                        }
-
-                    }
-
-                    caller->cleanGarbage();
-                    return SUCCESS;
-                }
-
-                if (vm.count("expected-result")) {
-                    string function = vm["expected-result"].as< string >();
-                    Map2Check::Log::Debug("Expected result: " + function );
-
-                    if (counterExample->getViolatedProperty() == function) {
-                        caller->cleanGarbage();
-                        return SUCCESS;
-                    }
-                    else {
-                        throw Map2Check::Exceptions::Map2CheckException("Unexpected result");
-                    }
-                }
-
-                caller->cleanGarbage();
-            }
-
-
-            po::notify(vm); // throws on error, so do after help in case
-            // there are any problems
-        }
-        catch(po::error& e)
-        {
-            Map2Check::Log::Fatal(e.what());
-            std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
-            std::cerr << desc << std::endl;
-            return ERROR_IN_COMMAND_LINE;
-        }
-
-        // application code here //
-
-    }
-    catch(std::exception& e)
-    {      
-        Map2Check::Log::Fatal(e.what());
-        std::cerr << "Unhandled Exception reached the top of main: "
-            << e.what() << ", application will now exit" << std::endl;
-        return ERROR_UNHANDLED_EXCEPTION;
-
-    }
-
-    return SUCCESS;
-
+  return SUCCESS;
 }

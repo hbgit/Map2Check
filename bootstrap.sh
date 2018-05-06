66f83931dd1945a55ed4be344bd596f276cdd053
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-05-06 08:27:47 -0300

    Updated script to let llvm 6 compile afl

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,87 +1,44 @@
 #!/bin/bash
 
-#set -x
-
 RUNDIR=`pwd`
-PREFIX=`pwd`/release
 
-map2check_get_deps() 
+# Check if LLVM is already downloaded
+# TODO: Check for distro before downloading
+get_llvm()
 {
-    cd $RUNDIR
-    if [ ! -d dependencies ]; then	
-		mkdir dependencies    
-    fi
-    cd dependencies
-   
-	# Check if LLVM is already downloaded
-	# TODO: Check for distro before downloading
+	
 	if [ ! -d clang ]; then	
 		echo "Clang not found, downloading it"		
 		curl http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz | tar -xJ
 		mv clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-16.04 clang
-    fi
-    cd -
+  fi
 }
 
-cp_utils_file() 
+# Check if AFL is already downloaded and build it using clang
+get_afl()
+{	
+	if [ ! -d afl-2.52b ]; then	
+		echo "AFL not found, downloading it"		
+		curl http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz | tar -xz
+		echo "Building AFL"
+		cd afl-2.52b
+		CC=../clang/bin/clang CXX=../clang/bin/clang++ make
+		cd ..
+  fi
+}
+
+map2check_get_deps() 
 {
-	cd $RUNDIR
-	
-	# Copying Map2Check license
-	if [ ! -f $PREFIX/LICENSE ]; then			
-		cp LICENSE $PREFIX/
-	else
-		rm $PREFIX/LICENSE		
-		cp LICENSE $PREFIX/
-    fi
-	
-	# Copying licenses tools
-	if [ ! -d $PREFIX/LICENSES ]; then	
-		mkdir $PREFIX/LICENSES
-		cp utils/*.TXT $PREFIX/LICENSES/
-	else
-		rm -rf $PREFIX/LICENSES
-		mkdir $PREFIX/LICENSES
-		cp utils/*.TXT $PREFIX/LICENSES/
-    fi
-    
-    # Wrapper script
-    if [ ! -f $PREFIX/map2check-wrapper.py ]; then	
-		cp utils/map2check-wrapper.py $PREFIX/
-	else
-		rm $PREFIX/map2check-wrapper.py
-		cp utils/map2check-wrapper.py $PREFIX/
-    fi
-    
-    # Samples to test map2check
-    if [ ! -d $PREFIX/sample ]; then	
-		mkdir $PREFIX/sample
-		cp -r sample/* $PREFIX/sample/
-	else
-		rm -rf $PREFIX/sample
-		mkdir $PREFIX/sample
-		cp -r sample/* $PREFIX/sample/
-    fi
-    
-    # tool-info benchexec
-    if [ ! -d $PREFIX/moduleBenchExec ]; then	
-		mkdir $PREFIX/moduleBenchExec/
-		cp -r utils/moduleBenchExec/* $PREFIX/moduleBenchExec/
-	else
-		rm -rf $PREFIX/moduleBenchExec
-		mkdir $PREFIX/moduleBenchExec/
-		cp -r utils/moduleBenchExec/* $PREFIX/moduleBenchExec/
-	fi    
-    
-    # README FILE
-    if [ ! -f $PREFIX/README.md ]; then	
-		cp README.md $PREFIX/
-	else
-		rm $PREFIX/README.md
-		cp README.md $PREFIX/
-	fi
-	
-	
+  cd $RUNDIR
+  if [ ! -d dependencies ]; then	
+		mkdir dependencies    
+ 	fi
+  cd dependencies
+   
+	 get_llvm
+	 get_afl
+	 echo "Everything OK!"
+	 cd -	
 }
 
 map2check_get_deps

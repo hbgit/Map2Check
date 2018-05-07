fae76946e38c489c6418e09de8151dea153e59f6
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-05-07 09:37:38 -0300

    Added AFL class test

--- test/afl_test.cpp
+++ test/afl_test.cpp
@@ -1,12 +1,80 @@
-#define BOOST_TEST_MODULE __AFL_TESTS
+#define BOOST_TEST_MODULE AFL_TESTS
 #include <boost/test/included/unit_test.hpp>
+#include <modules/frontend/utils/afl.hpp>
 
-// BOOST_AUTO_TEST_CASE(FailTest)
-// {
-//     BOOST_CHECK_EQUAL(5, 3 + 1);
-// }
+// Test for default constructor
+BOOST_AUTO_TEST_CASE(constructors) {
+  std::string path = "/my-custom-path/";
+  std::string expectedCompilerCommand =
+    path + "afl-clang";
 
-BOOST_AUTO_TEST_CASE(PassTest) {
-    BOOST_CHECK_EQUAL(4, 2 * 2);
+  std::string expectedExecuterCommand =
+    path + "afl-fuzz";
+
+  Map2Check::AFL afl(path);
+  BOOST_CHECK_EQUAL(afl.getCompilerCommand(), expectedCompilerCommand);
+  BOOST_CHECK_EQUAL(afl.getExecutionCommand(), expectedExecuterCommand);
 }
 
+BOOST_AUTO_TEST_CASE(_32Bits_Compiler) {
+  std::string path = "/my-custom-path/";
+  std::string expectedCompilerCommand =
+    path + "afl-clang -m32";
+
+  Map2Check::AFL_COMPILE compiler;
+  compiler.setPath(path);
+  compiler.setCompileTo32Bits(true);
+
+  BOOST_CHECK_EQUAL(expectedCompilerCommand, compiler.getCommand());
+}
+
+BOOST_AUTO_TEST_CASE(_Timeout_Executor) {
+  std::string path = "/my-custom-path/";
+  std::string expectedExecutorCommand =
+    "timeout 30 " + path + "afl-fuzz";
+
+  Map2Check::AFL_EXEC executor;
+  executor.setPath(path);
+  executor.setTimeout(30);
+
+  BOOST_CHECK_EQUAL(expectedExecutorCommand, executor.getCommand());
+}
+
+BOOST_AUTO_TEST_CASE(_Chrash_Executor) {
+  std::string path = "/my-custom-path/";
+  std::string expectedExecutorCommand =
+    "AFL_BENCH_UNTIL_CRASH=1 " + path + "afl-fuzz";
+
+  Map2Check::AFL_EXEC executor;
+  executor.setPath(path);
+  executor.setBenchUntilCrash(true);
+
+  BOOST_CHECK_EQUAL(expectedExecutorCommand, executor.getCommand());
+}
+
+BOOST_AUTO_TEST_CASE(_Skip_Cpu_Executor) {
+  std::string path = "/my-custom-path/";
+  std::string expectedExecutorCommand =
+    "AFL_SKIP_CPUFREQ=1 " + path + "afl-fuzz";
+
+  Map2Check::AFL_EXEC executor;
+  executor.setPath(path);
+  executor.setSkipCpuFreq(true);
+
+  BOOST_CHECK_EQUAL(expectedExecutorCommand, executor.getCommand());
+}
+
+BOOST_AUTO_TEST_CASE(_Change_Mode_Executor) {
+  std::string path = "/my-custom-path/";
+  std::string expectedExecutorCommand =
+    "AFL_BENCH_UNTIL_CRASH=1 AFL_SKIP_CPUFREQ=1 timeout 30 " +
+    path + "afl-fuzz";
+
+  Map2Check::AFL_EXEC executor;
+  executor.setPath(path);
+  executor.setBenchUntilCrash(true);
+  executor.setTimeout(30);
+  executor.setSkipCpuFreq(true);
+
+  BOOST_CHECK_EQUAL(expectedExecutorCommand, executor.getCommand());
+}

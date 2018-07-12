b524b082a5342edc562a04efc3df866c3c16f601
Rafael Sa Menezes <rafa.sa.xp@gmail.com>
2018-07-12 12:46:18 -0300

    Changed from qt project file to CMake

--- modules/frontend/caller.cpp
+++ modules/frontend/caller.cpp
@@ -1,181 +1,133 @@
 #include "caller.hpp"
 
-#include <stdlib.h>
-// CPP Libs
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <regex>
-// #include <boost/filesystem.hpp>
+#include <QDebug>
+#include <QSysInfo>
+#include <QStringList>
+#include <QProcess>
+#include <QFile>
 
+namespace {
+inline QString getOSSuffix()
+{
+    return QSysInfo::kernelType() == "winnt" ? ".exe" : "";
+}
 
-#include "utils/log.hpp"
-#include "utils/tools.hpp"
-
-// namespace fs = boost::filesystem;
-// }  // namespace
-
+inline QString getHashedName(QString filename)
+{
+    return "compiled";
+}
+}
 
 namespace Map2Check {
-Caller::Caller(std::string bcprogram_path) {
-  this->cleanGarbage();
-  this->pathprogram = bcprogram_path;
+
+inline QString Caller::getPostOptimizationFlags()
+{
+    return "-O3";
 }
 
-std::string Caller::preOptimizationFlags() {
-  std::ostringstream flags;
-  flags.str("");
-  flags << "-O0";
-  return flags.str();
-}
-
-std::string Caller::postOptimizationFlags() {
-  std::ostringstream flags;
-  flags.str("");
-  flags << "-O3";
-  return flags.str();
-}
-
-void Caller::cleanGarbage() {
-  std::ostringstream removeCommand;
-  removeCommand.str("");
-  removeCommand << "rm -rf"
-              << " klee-*"
-              << " *.log"
-              << " list-*"
-              << " *.csv"
-              << " map2check_property"
-              << " automata_list_log.st"
-              << " track_bb_log.st"
-              << " map2check_property_klee_unknown"
-              << " map2check_property_klee_deref"
-              << " map2check_property_klee_memtrack"
-              << " map2check_property_overflow"
-              << " map2check_property_klee_free"
-              << " preprocessed.c"
-              << " optimized.bc"
-              << " output.bc"
-              << " witnessInfo";
-
-  system(removeCommand.str().c_str());
-}
-
-int Caller::callPass(Map2CheckMode mode, bool sv_comp) {
-  std::ostringstream transformCommand;
-  transformCommand.str("");
-  transformCommand << Map2Check::optBinary;
-  /* TODO(rafa.sa.xp@gmail.com): Should apply generate_automata_true 
-   *                             and TrackBasicBlockPass now */
-  transformCommand << " -load ${MAP2CHECK_PATH}/lib/libNonDetPass.so "
-    << "-non_det < entry.bc > non_det.bc";
-
-  system(transformCommand.str().c_str());
-
-  transformCommand.str("");
-  transformCommand << Map2Check::optBinary;
-  /* TODO(rafa.sa.xp@gmail.com): Should apply generate_automata_true 
-   *                             and TrackBasicBlockPass now */
-  transformCommand << " -load ${MAP2CHECK_PATH}/lib/libMap2CheckLibrary.so "
-    << "-map2check < non_det.bc > output.bc";
-
-  system(transformCommand.str().c_str());
-
-  return 1;
+inline QString Caller::getPreOptimizationFlags()
+{
+    return "-O0";
 }
 
 
-int Caller::callPass(Map2CheckMode mode, std::string target_function,
-                     bool sv_comp) {
-  return -1;
+void Caller::compileCFile()
+{
+    QString filename = program;
+    QString output = getHashedName(program);
+    qDebug() << "Started compilation";
+
+    emit instrumentationUpdate(InstrumentationStatus::CompilationStart);
+    QString clang = "clang";
+    clang.append(getOSSuffix());
+
+    // TODO: Add include folder on project
+    QStringList arguments;
+    arguments << "-I include";
+    arguments << "-Wno-everything";
+    arguments << "-Winteger-overflow";
+    arguments << "-c";
+    arguments << "-emit-llvm";
+    arguments << "-g";
+    arguments << Caller::getPreOptimizationFlags();
+    arguments << "-o" << output.append(".bc");
+    arguments << filename;
+
+    QProcess *process = new QProcess(this);
+    QObject::connect(process,  QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+                     [=](int, QProcess::ExitStatus exitStatus)
+    {
+        if(exitStatus == QProcess::CrashExit)
+        {
+            emit error("Error executing clang");
+            finished();
+        }
+        else
+        {
+            qDebug() << "Messages from compilation";
+            QString CompilationErrors(process->readAllStandardError());
+            if(!CompilationErrors.isEmpty())
+            {
+                emit error(CompilationErrors.toStdString().c_str());
+                finished();
+            }
+            instrumentPass();
+        }
+    });
+    process->start(clang, arguments);
 }
 
-void Caller::linkLLVM() {
-  /* Link functions called after executing the passes */
-  // TODO(rafa.sa.xp@gmail.com) Only link against used libraries
+void Caller::instrumentPass()
+{
+    // TODO: check other modes
+     QString opt = "opt";
+     QStringList arguments;
+     // Should fix for all OS (.so is for *nix)
+     arguments << "-load ${MAP2CHECK_PATH}/lib/libNonDetPass.so";
 
-  std::ostringstream linkCommand;
-  linkCommand.str("");
-  linkCommand << Map2Check::llvmLinkBinary;
-  linkCommand
-      << " output.bc"
-      << " ${MAP2CHECK_PATH}/lib/Map2CheckFunctions.bc"
-      << " ${MAP2CHECK_PATH}/lib/AllocationLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/HeapLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/TrackBBLog.bc"
-      // << " ${MAP2CHECK_PATH}/lib/BTree.bc"
-      << " ${MAP2CHECK_PATH}/lib/Container.bc"
-      << " ${MAP2CHECK_PATH}/lib/KleeLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/ListLog.bc"
-      << " ${MAP2CHECK_PATH}/lib/PropertyGenerator.bc"
-      // << " ${MAP2CHECK_PATH}/lib/BinaryOperation.bc "
-      << "  > result.bc";
+     QProcess *process = new QProcess(this);
+     QObject::connect(process,  QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+                      [=](int, QProcess::ExitStatus exitStatus)
+     {
+         // TODO: implement
+         qDebug() << "opt executed";
+         finished();
+     });
+     QString output = getHashedName(program);
 
-  system(linkCommand.str().c_str());
+     QFile file(output);
+     if (!file.open(QIODevice::ReadOnly))
+     {
+         // Error opening file
+         emit error("Could not open compiled .bc file");
+         finished();
+     }
 
-  // TODO(rafa.sa.xp@gmail.com) Check how optimization can generate errors
-  std::ostringstream optimizeCommand;
-  optimizeCommand.str("");
-  optimizeCommand << Map2Check::optBinary;
-  optimizeCommand << " " << Caller::postOptimizationFlags()
-           << " result.bc > optimized.bc";
+     while(!file.atEnd())
+     {
 
-}
-
-void Caller::executeAnalysis() {
+     }
 
 }
 
-std::vector<int> Caller::processClangOutput() {
-  const char* path_name = "clang.out";
-
-  std::vector<int> result;
-
-  ifstream in(path_name);
-  if (!in.is_open()) {
-    Map2Check::Log::Debug("Clang did not generate warning or errors");
-    return result;
-  }
-  Map2Check::Log::Debug("Clang generate warning or errors");
-
-  regex overflowWarning(".*:([[:digit:]]+):[[:digit:]]+:.*Winteger-overflow.*");
-  string line;
-  smatch match;
-  while (getline(in, line)) {
-    if (std::regex_search(line, match, overflowWarning) && match.size() > 1) {
-      Map2Check::Log::Info("Found warning at line " + match[1].str());
-      int lineNumber = std::stoi(match[1].str());
-      result.push_back(lineNumber);
-    }
-  }
-
-  return result;
+void Caller::analyzeProgram(QString program, Map2CheckMode mode, QString targetFunction)
+{
+    this->program = program;
+    this->mode = mode;
+    this->targetFunction = targetFunction;
+    emit instrumentationUpdate(InstrumentationStatus::Started);
+    // Should execute after cleaning the file with regex
+    compileCFile();
 }
 
-string Caller::compileCFile(std::string cprogram_path) {
-  Map2Check::Log::Info("Compiling " + cprogram_path);
+void Caller::prematureStop()
+{
+    removeTemporaryFiles();
+}
 
-  std::ostringstream commandRemoveExternMalloc;
-  commandRemoveExternMalloc.str("");
-  commandRemoveExternMalloc << "cat " << cprogram_path << " | ";
-  commandRemoveExternMalloc << "sed -e 's/.*extern.*malloc.*//g' "
-                            << "  -e 's/.*void \\*malloc(size_t size).*//g' "
-                            <<" > preprocessed.c";
+void Caller::removeTemporaryFiles()
+{
 
-  system(commandRemoveExternMalloc.str().c_str());
-
-  std::ostringstream command;
-  command.str("");
-  command << Map2Check::clangBinary << " -I"
-          << Map2Check::clangIncludeFolder
-          << " -Wno-everything "
-          << " -Winteger-overflow "
-          << " -c -emit-llvm -g"
-          << " " << Caller::preOptimizationFlags()
-          << " -o compiled.bc "
-          << "preprocessed.c"
-          << " > clang.out 2>&1";
-
-  system(command.str().c_str());
-  return ("compiled.bc");
 }
 } // namespace Map2Check
+

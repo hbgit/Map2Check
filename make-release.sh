f68e22a6fe2cb259ae274319780858b3d3bd209d
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-07-19 22:25:17 -0300

    Removed afl

--- make-release.sh
+++ make-release.sh
@@ -31,15 +31,6 @@ cp ./dependencies/clang/bin/opt ./release/bin/opt
 cp ./dependencies/clang/bin/llvm-link ./release/bin/llvm-link
 cp -r ./dependencies/clang/lib/clang/6.0.0/include ./release
 
-cp -r ./dependencies/afl-2.52b/ ./release/afl
-
-echo ""
-echo "Creating default dictionaries... "
-echo ""
-
-mkdir -p ./release/in
-echo "0" > ./release/in/dict0
-
 echo ""
 echo "DONE"
 echo ""

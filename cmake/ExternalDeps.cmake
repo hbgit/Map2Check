fbf5342330ff5963d5a10579c21f205cedb3f25f
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-07-21 19:52:22 -0300

    Added binary installation with permission

--- cmake/ExternalDeps.cmake
+++ cmake/ExternalDeps.cmake
@@ -5,17 +5,29 @@
 # Works on: Ubuntu 14.04, 16.04, 18.04, Fedora, OpenSuse
 # Did not work on: Alpine
 
-set(DOCKER_USR_LIB_PATH "/usr/lib/x86_64-linux-gnu/")
-
-function(INSTALL_LINK_FILE link dest name)
-  execute_process(COMMAND readlink -f ${link}
+function(install_link_file LINK DEST NAME)
+  execute_process(COMMAND readlink -f ${LINK}
     OUTPUT_VARIABLE EXTERNAL_LIB
     OUTPUT_STRIP_TRAILING_WHITESPACE)
   install(FILES ${EXTERNAL_LIB}
-    DESTINATION ${dest}
-    RENAME ${name})
-endfunction(INSTALL_LINK_FILE)
-  
+    DESTINATION ${DEST}
+    RENAME ${NAME})
+endfunction(install_link_file)
+
+function(install_exec_file LINK DEST NAME)
+  execute_process(COMMAND readlink -f ${LINK}
+    OUTPUT_VARIABLE EXTERNAL_LIB
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  install(FILES ${EXTERNAL_LIB}
+    PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
+    DESTINATION ${DEST}
+    RENAME ${NAME})
+endfunction(install_exec_file)
+
+
+set(DOCKER_USR_LIB_PATH "/usr/lib/x86_64-linux-gnu/")
+
+#Qt deps
 list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libQt5Core.so.5")
 list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libstdc++.so.6")
 list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libicui18n.so.55")
@@ -24,13 +36,23 @@ list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libpcre2-16.so.0")
 list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libdouble-conversion.so.1")
 list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libicudata.so.55")
 
+#Clang deps
+list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libLLVM-6.0.so.1")
+list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libjsoncpp.so.1")
+list(APPEND MAP2CHECK_EXTERNAL_USR_LIBS "libedit.so.2")
+
+
 foreach(L ${MAP2CHECK_EXTERNAL_USR_LIBS})
   set(LIB ${DOCKER_USR_LIB_PATH}${L})
-  INSTALL_LINK_FILE(${LIB} lib ${L})
+  install_link_file(${LIB} lib ${L})
 endforeach()
 
 set(DOCKER_LIB_PATH "/lib/x86_64-linux-gnu/")
+#Qt libs
 list(APPEND MAP2CHECK_EXTERNAL_LIBS "libglib-2.0.so.0")
+
+#Clang libs
+list(APPEND MAP2CHECK_EXTERNAL_LIBS "libtinfo.so.5")
 
 # Probably not needed
 #list(APPEND MAP2CHECK_EXTERNAL_LIBS "libpcre.so.3")
@@ -39,5 +61,15 @@ list(APPEND MAP2CHECK_EXTERNAL_LIBS "libglib-2.0.so.0")
 
 foreach(L ${MAP2CHECK_EXTERNAL_LIBS})
   set(LIB ${DOCKER_LIB_PATH}${L})
-  INSTALL_LINK_FILE(${LIB} lib ${L})
+  install_link_file(${LIB} lib ${L})
+endforeach()
+
+
+set(DOCKER_USR_BIN_PATH "/usr/bin/")
+list(APPEND MAP2CHECK_EXTERNAL_USR_BIN "clang-6.0")
+list(APPEND MAP2CHECK_EXTERNAL_USR_BIN "opt-6.0")
+
+foreach(B ${MAP2CHECK_EXTERNAL_USR_BIN})
+  set(BIN ${DOCKER_USR_BIN_PATH}${B})
+  install_exec_file(${BIN} bin ${B})
 endforeach()

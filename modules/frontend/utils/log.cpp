1f41efd4538b9d0732d8d1e996f5c30bf1ea7740
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-07-30 16:51:01 -0300

    Added endline after printing log string

--- modules/frontend/utils/log.cpp
+++ modules/frontend/utils/log.cpp
@@ -5,23 +5,23 @@
 namespace Map2Check {
 
 void Log::Error(std::string msg) {
-  std::cout << "\033[0;31m" << msg << "\033[0m";
+  std::cout << "\033[0;31m" << msg << "\033[0m\n";
 }
 
 void Log::Warning(std::string msg) {
-  std::cout << "\033[1;33m" << msg << "\033[0m";
+  std::cout << "\033[1;33m" << msg << "\033[0m\n";
 }
 
 void Log::Debug(std::string msg) {
-  std::cout << "\033[0;33m" << msg << "\033[0m";
+  std::cout << "\033[0;33m" << msg << "\033[0m\n";
 }
 
 void Log::Info(std::string msg) {
-  std::cout << "\033[0;32m" << msg << "\033[0m";
+  std::cout << "\033[0;32m" << msg << "\033[0m\n";
 }
 
 void Log::Fatal(std::string msg) {
-  std::cout << "\033[0;31m" << msg << "\033[0m";
+  std::cout << "\033[0;31m" << msg << "\033[0m\n";
 }
 
 }  // namespace Map2Check

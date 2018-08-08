6ce57ebae6c3a0711e0b94affd174089da796654
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-08-08 10:23:30 -0300

    Added cmake module to download test cases

---
+++ cmake/DownloadRegression.cmake
@@ -0,0 +1,11 @@
+# TODO: rename deref to SVCOMP
+list(APPEND MAP2CHECK_REGRESSION "DEREF")
+
+foreach(R ${MAP2CHECK_REGRESSION})
+  include(${PROJECT_SOURCE_DIR}/cmake/DownloadRegression${R}.cmake)
+  foreach(F ${MAP2CHECK_REGRESSION_${R}})
+    file(DOWNLOAD map2check_regression_${F} tests/${R}/${F} SHOW_PROGRESS)
+  endforeach()
+endforeach()
+
+install(DIRECTORY ${PROJECT_BINARY_DIR}/tests/ DESTINATION tests/)
---
+++ cmake/DownloadRegressionDEREF.cmake
@@ -0,0 +1,10 @@
+# TODO: Rename file to SVCOMP
+# Add all links to .c/.i programs with FALSE-DEREF
+list(APPEND MAP2CHECK_REGRESSION_DEREF "add_last_unsage_false-valid-deref.i")
+set(map2check_regression_add_last_unsage_false-valid-deref.i "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/array-memsafety/add_last_unsafe_false-valid-deref.i")
+
+list(APPEND MAP2CHECK_REGRESSION_DEREF "bubblesort_unsafe_false-valid-deref.i")
+set(map2check_regression_bubblesort_unsafe_false-valid-deref.i "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/array-memsafety/bubblesort_unsafe_false-valid-deref.i")
+
+list(APPEND MAP2CHECK_REGRESSION_DEREF "cstrncpy_unsafe_false-valid-deref.i")
+set(map2check_regression_cstrncpy_unsafe_false-valid-deref.i "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/array-memsafety/cstrncpy_unsafe_false-valid-deref.i")

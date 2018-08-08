e7593004d007d98b3ce64994f69af11a20de9f5c
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-08-08 18:21:10 -0300

    Added deref regression tests

--- cmake/DownloadRegression.cmake
+++ cmake/DownloadRegression.cmake
@@ -1,10 +1,11 @@
 # TODO: rename deref to SVCOMP
+set(SVCOMP_BASE "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/")
 list(APPEND MAP2CHECK_REGRESSION "DEREF")
 
 foreach(R ${MAP2CHECK_REGRESSION})
   include(${PROJECT_SOURCE_DIR}/cmake/DownloadRegression${R}.cmake)
   foreach(F ${MAP2CHECK_REGRESSION_${R}})
-    file(DOWNLOAD map2check_regression_${F} tests/${R}/${F} SHOW_PROGRESS)
+    file(DOWNLOAD ${map2check_regression_${F}} tests/${R}/${F} SHOW_PROGRESS)
   endforeach()
 endforeach()
 
--- cmake/DownloadRegressionDEREF.cmake
+++ cmake/DownloadRegressionDEREF.cmake
@@ -1,10 +1,92 @@
-# TODO: Rename file to SVCOMP
+
+
 # Add all links to .c/.i programs with FALSE-DEREF
+
 list(APPEND MAP2CHECK_REGRESSION_DEREF "add_last_unsage_false-valid-deref.i")
-set(map2check_regression_add_last_unsage_false-valid-deref.i "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/array-memsafety/add_last_unsafe_false-valid-deref.i")
+set(map2check_regression_add_last_unsage_false-valid-deref.i "${SVCOMP_BASE}array-memsafety/add_last_unsafe_false-valid-deref.i")
 
 list(APPEND MAP2CHECK_REGRESSION_DEREF "bubblesort_unsafe_false-valid-deref.i")
-set(map2check_regression_bubblesort_unsafe_false-valid-deref.i "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/array-memsafety/bubblesort_unsafe_false-valid-deref.i")
+set(map2check_regression_bubblesort_unsafe_false-valid-deref.i "${SVCOMP_BASE}/array-memsafety/bubblesort_unsafe_false-valid-deref.i")
 
 list(APPEND MAP2CHECK_REGRESSION_DEREF "cstrncpy_unsafe_false-valid-deref.i")
-set(map2check_regression_cstrncpy_unsafe_false-valid-deref.i "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/array-memsafety/cstrncpy_unsafe_false-valid-deref.i")
+set(map2check_regression_cstrncpy_unsafe_false-valid-deref.i "${SVCOMP_BASE}/array-memsafety/cstrncpy_unsafe_false-valid-deref.i")
+
+list(APPEND MAP2CHECK_REGRESSION_DEREF "cstrncpy_unsafe_false-valid-deref.i")
+set(map2check_regression_cstrncpy_unsafe_false-valid-deref.i "${SVCOMP_BASE}/array-memsafety/cstrncpy_unsafe_false-valid-deref.i")
+
+list(APPEND MAP2CHECK_REGRESSION_DEREF "memleaks_test14_3_false-valid-deref.i")
+set(map2check_regression_memleaks_test14_3_false-valid-deref.i "${SVCOMP_BASE}/ldv-memsafety/memleaks_test14_3_false-valid-deref.i")
+
+list(APPEND MAP2CHECK_REGRESSION_DEREF "memleaks_test22_3_false-valid-deref.i")
+set(map2check_regression_memleaks_test22_3_false-valid-deref.i "${SVCOMP_BASE}/ldv-memsafety/memleaks_test22_3_false-valid-deref.i")
+
+list(APPEND MAP2CHECK_REGRESSION_DEREF "memleaks_test22_5_false-valid-deref.i")
+set(map2check_regression_memleaks_test22_5_false-valid-deref.i "${SVCOMP_BASE}/ldv-memsafety/memleaks_test22_5_false-valid-deref.i")
+
+#relax_false-valid-deref.i
+
+# memleaks_test11_1_false-valid-free.i
+# memleaks_test12_false-valid-free.i
+# memleaks_test17_2_false-valid-free.i
+# memleaks_test19_false-valid-free.i
+# memleaks_test1_false-valid-free.i
+# #memleaks_test3_false-valid-free.i
+# memleaks_test6_2_false-valid-free.i
+# memleaks_test8_2_false-valid-free.i
+
+# memleaks_test10_false-valid-memtrack_true-termination.i
+# memleaks_test11_2_false-valid-memtrack_true-termination.i
+# memleaks_test13_1_false-valid-memtrack_true-termination.i
+# memleaks_test13_2_false-valid-memtrack.i
+# memleaks_test14_1_false-valid-memtrack_true-termination.i
+# memleaks_test14_2_false-valid-memtrack.i
+# memleaks_test15_false-valid-memtrack.i
+# memleaks_test16_1_false-valid-memtrack_true-termination.i
+# memleaks_test16_2_false-valid-memtrack_true-termination.i
+# memleaks_test17_1_false-valid-memtrack.i
+# memleaks_test17_3_false-valid-memtrack.i
+# memleaks_test18_1_false-valid-memtrack_true-termination.i
+# memleaks_test18_2_false-valid-memtrack_true-termination.i
+# memleaks_test18_3_false-valid-memtrack_true-termination.i
+# memleaks_test1_false-valid-memtrack_true-termination.i
+# memleaks_test20_false-valid-memtrack_true-termination.i
+# memleaks_test21_false-valid-memtrack_true-termination.i
+# memleaks_test22_1_false-valid-memtrack_true-termination.i
+# memleaks_test22_2_false-valid-memtrack_true-termination.i
+# memleaks_test22_4_false-valid-memtrack_true-termination.i
+# memleaks_test4_false-valid-memtrack_true-termination.i
+# memleaks_test5_1_false-valid-memtrack_true-termination.i
+# memleaks_test5_2_false-valid-memtrack_true-termination.i
+# memleaks_test6_3_false-valid-memtrack_true-termination.i
+# memleaks_test7_false-valid-memtrack_true-termination.i
+# memleaks_test8_1_false-valid-memtrack_true-termination.i
+# memleaks_test9_1_false-valid-memtrack_true-termination.i
+# memleaks_test9_2_false-valid-memtrack_true-termination.i
+
+
+
+# data_structures_set_multi_proc_false-unreach-call_ground.i
+# sorting_selectionsort_false-unreach-call2_ground.i
+# sorting_selectionsort_false-unreach-call_ground.i
+# standard_copy1_false-unreach-call_ground.i
+# standard_copy2_false-unreach-call_ground.i
+# standard_copy3_false-unreach-call_ground.i
+# standard_copy4_false-unreach-call_ground.i
+# standard_copy5_false-unreach-call_ground.i
+# standard_copy6_false-unreach-call_ground.i
+# standard_copy7_false-unreach-call_ground.i
+# standard_copy8_false-unreach-call_ground.i
+# standard_copy9_false-unreach-call_ground.i
+# standard_copyInitSum2_false-unreach-call_ground.i
+# standard_init1_false-unreach-call_ground.i
+# standard_init2_false-unreach-call_ground.i
+# standard_init3_false-unreach-call_ground.i
+# standard_init4_false-unreach-call_ground.i
+# standard_init5_false-unreach-call_ground.i
+# standard_init6_false-unreach-call_ground.i
+# standard_init7_false-unreach-call_ground.i
+# standard_init8_false-unreach-call_ground.i
+# standard_init9_false-unreach-call_ground.i
+# standard_minInArray_false-unreach-call_ground.i
+# standard_partition_false-unreach-call_ground.i
+# standard_running_false-unreach-call.i

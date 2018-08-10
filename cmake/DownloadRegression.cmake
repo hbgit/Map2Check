c32c6ad76526fdb5ebaa028811439274d4cf755d
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-08-10 10:38:34 -0300

    Added condition to download file

--- cmake/DownloadRegression.cmake
+++ cmake/DownloadRegression.cmake
@@ -1,4 +1,3 @@
-# TODO: rename deref to SVCOMP
 set(SVCOMP_BASE "https://raw.githubusercontent.com/sosy-lab/sv-benchmarks/master/c/")
 list(APPEND MAP2CHECK_REGRESSION "DEREF")
 list(APPEND MAP2CHECK_REGRESSION "FREE")
@@ -9,7 +8,9 @@ foreach(R ${MAP2CHECK_REGRESSION})
   include(${PROJECT_SOURCE_DIR}/cmake/DownloadRegression${R}.cmake)
   foreach(F ${MAP2CHECK_REGRESSION_${R}})
     message("Downloading ${F}")
-    file(DOWNLOAD ${map2check_regression_${F}} ${PROJECT_SOURCE_DIR}/regression/${R}/${F} SHOW_PROGRESS)
+    if(NOT EXISTS ${PROJECT_SOURCE_DIR}/tests/regression/${R}/${F})
+      file(DOWNLOAD ${map2check_regression_${F}} ${PROJECT_SOURCE_DIR}/tests/regression/${R}/${F} SHOW_PROGRESS)
+    endif()
   endforeach()
 endforeach()
 

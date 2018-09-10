83b374f5469287ef66c05ef780589eb8841cb223
Herbert Rocha <herberthb12@gmail.com>
2018-09-10 17:24:07 -0400

    Added new tests
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

---
+++ tests/regression/ASSERT/ex01_false.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+	int i=1;
+	int n = __VERIFIER_nondet_int();
+
+	while(i<n) {
+		i++;
+	}
+	__VERIFIER_assert(i>n);
+}
---
+++ tests/regression/ASSERT/ex02_false.c
@@ -0,0 +1,16 @@
+int main(void)
+{
+	int k=1;
+	int i=1;
+	int j=0;
+	int n = __VERIFIER_nondet_int();
+	while(i<n) {
+		j=0;
+		while(j<i) {
+			k += (i-j);
+			j++;
+		}
+		i++;
+	}
+	__VERIFIER_assert(k==0);
+}

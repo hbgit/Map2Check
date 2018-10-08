378bfd41386f5e464673fc3ed191fabea4d66b3d
Map2Check <map2check@domain.com>
2018-10-08 15:45:38 +0000

    Minor changes
    Signed-off-by: Map2Check <map2check@domain.com>

--- utils/build_crabllvm.py
+++ utils/build_crabllvm.py
@@ -15,33 +15,25 @@ if not FORCE_BUILD and os.path.isdir("build/dependencies/crab-llvm/build/run"):
 
 os.chdir("build/dependencies/")
 
-#if not os.path.isdir("clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04"):
-#	os.system("wget http://releases.llvm.org/3.8.0/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz")
-#	os.system("tar -xf clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz")
+#llvm_dir=os.path.dirname(os.path.abspath("../../LLVM-3.8-Linux.tar.gz"))
+#print(llvm_dir)
+#llvm_dir = llvm_dir + "/LLVM-3.8-Linux/share/llvm/cmake"
+llvm_dir = "/llvm/release/llvm600/lib/cmake/llvm/"
 
-llvm_dir=os.path.dirname(os.path.abspath("../../LLVM-3.8-Linux.tar.gz"))
-##print(llvm_dir)
-##os.system("export LLVM_DIR="+llvm_dir+"/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04/share/llvm/cmake")
-##os.system("echo $LLVM_DIR")
-llvm_dir = llvm_dir + "/LLVM-3.8-Linux/share/llvm/cmake"
-print(llvm_dir)
 
 if not os.path.isdir("crab-llvm"):
-	os.system("git clone https://github.com/seahorn/crab-llvm.git")
+	os.system("git clone https://github.com/hbgit/crab-llvm.git")
 
 os.chdir("crab-llvm")
-# TODO define commit to build
+# Defining brach to build
+os.system("git checkout dev-llvm-6.0")
 
 if not os.path.isdir("build"):
 	os.system("mkdir build")
 
 os.chdir("build")
 
-print("cmake -DLLVM_DIR=" + llvm_dir + " -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_PROGRAM_PATH=/usr/bin -DCMAKE_INSTALL_PREFIX=run -DUSE_LDD=ON -DUSE_APRON=ON ../")
-
-os.system("cmake -DLLVM_DIR=" + llvm_dir + " -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_PROGRAM_PATH=/usr/bin -DCMAKE_INSTALL_PREFIX=run -DUSE_LDD=ON -DUSE_APRON=ON ../")
-
-#os.system("cmake -DCMAKE_INSTALL_PREFIX=run -DCMAKE_BUILD_TYPE= -DUSE_LDD=ON -DUSE_APRON=ON ../")
+print("cmake -DLLVM_DIR=" + llvm_dir + " -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=_DIR_ -DUSE_LDD=ON -DUSE_APRON=ON ../")
 
 # cmake --build . --target extra 
 os.system("cmake --build . --target crab && cmake ..")

c84d5446504d87b356c4f4a060db6db7bf6800a9
Herbert Rocha <herberthb12@gmail.com>
2018-11-05 00:40:28 +0000

    Change solver to Z3
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- modules/frontend/caller.cpp
+++ modules/frontend/caller.cpp
@@ -276,7 +276,7 @@ void Caller::executeAnalysis() {
                   << " --allow-external-sym-calls"
                   << " -exit-on-error-type=Abort"
                   << " --optimize "
-                  << " -solver-backend=stp "
+                  << " -solver-backend=z3 "
                   << " -libc=uclibc"
                   << " ./" + programHash + "-witness-result.bc"
                   << "  > ExecutionOutput.log";

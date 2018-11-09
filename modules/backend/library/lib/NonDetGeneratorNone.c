ce54b3675f1c1f4e7c95a7dbe3869f420bbc11e9
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2018-11-09 17:15:37 -0200

    Implemented nondet_assume

--- modules/backend/library/lib/NonDetGeneratorNone.c
+++ modules/backend/library/lib/NonDetGeneratorNone.c
@@ -10,31 +10,21 @@ int main() {
   return 0;
 }
 
-int map2check_non_det_int() {
-  return 0;
-}
+int map2check_non_det_int() { return 0; }
 
-unsigned int map2check_non_det_uint() {
-  return 0;
-}
+unsigned int map2check_non_det_uint() { return 0; }
 
-char map2check_non_det_char() {
-  return 0;
-}
+char map2check_non_det_char() { return 0; }
 
-unsigned short map2check_non_det_ushort() {
-  return 0;
-}
+unsigned short map2check_non_det_ushort() { return 0; }
 
 void* map2check_non_det_pointer() {
   // Ignore warnings from this, it is desired to be undefined behaviour
   return 0;
 }
 
-long map2check_non_det_long() {
-  return 0;
-}
-
-void nondet_init() { }
-void nondet_destroy() { }
-void nondet_generate_aux_witness_files() { }
+long map2check_non_det_long() { return 0; }
+void nondet_assume(int expr) {}
+void nondet_init() {}
+void nondet_destroy() {}
+void nondet_generate_aux_witness_files() {}

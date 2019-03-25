4ed6105f88ead6dee516666fa34815ab9e17ce2e
Herbert Rocha <herberthb12@gmail.com>
2019-03-25 16:02:50 -0400

    Update travis to run regression testing
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- make-regression-test.sh
+++ make-regression-test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Checking if testing is adopting travis service
+xml_doc_benchexec_name="map2check_regression_test.xml"
+if [ "$1" != "" ]; then
+    xml_doc_benchexec_name="map2check_regression_test_travis.xml"
+fi
+
 # Check if docker image is already build
 # 1 - Build docker image available at https://github.com/hbgit/benchexecrun
 has_docker_img=`docker images | grep -c "hrocha/benchexecrun"`
@@ -10,7 +16,7 @@ if [ $has_docker_img -gt 0 ]; then
   if [ $is_main_dir -gt 0 ]; then
     docker run --rm -v /sys/fs/cgroup:/sys/fs/cgroup:rw \
     -v $(pwd):/home/bench/benchexec_files:Z hrocha/benchexecrun \
-    /bin/bash -c "cd release; python3 -m benchexec.benchexec --no-container ../tests/regression_test/xml_doc_benchexec/map2check_regression_test.xml"
+    /bin/bash -c "cd release; python3 -m benchexec.benchexec --no-container ../tests/regression_test/xml_doc_benchexec/$xml_doc_benchexec_name"
   else
     echo "Sorry, you should be in the main directory of the tool"
   fi

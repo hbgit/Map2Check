4093d911c0535410819d821604372067fe023947
Herbert Rocha <herberthb12@gmail.com>
2019-04-02 14:14:46 -0400

    Update check style script
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- check_code_style.py
+++ check_code_style.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-# Usage: ./check_code_style.sh [C|CPP]
+# Usage: ./check_code_style.sh [c|p]
 
 import os
 import argparse
+import re
 
 # Options
 parser = argparse.ArgumentParser()
@@ -23,11 +24,25 @@ if version == True:
   exit(0)
 
 if c_style == True:
-  print("C")
+  print("Formatting C codes with clang-format adopting LLVM style ...")
+
+  # File Extension filter. You can add new extension
+  cpp_extensions = (".c", ".h", ".hh")
+
+  # Apply the clang formatting
+  # Please note "-style" is for standard style options
+  # and "-i" is in-place editing
+  for root, dirs, files in os.walk("modules/backend"):
+      for file in files:
+          if file.endswith(cpp_extensions):
+              root = re.escape(root)
+              file = re.escape(file)
+              # print(root + "/" + file)
+              os.system("clang-format -i -style=google " + root + "/" + file)
   exit(0)
 
 if cpp_style == True:
-  print("Formatting C++ codes with clang-format ...")
+  print("Formatting C++ codes with clang-format adopting Google Style ...")
 
   # File Extension filter. You can add new extension
   cpp_extensions = (".cxx",".cpp",".c", ".hxx", ".hh", ".cc", ".hpp")
@@ -38,22 +53,18 @@ if cpp_style == True:
   for root, dirs, files in os.walk("modules/frontend"):
       for file in files:
           if file.endswith(cpp_extensions):
-              print(root + "/" + file)
-              #os.system("clang-format -i -style=google " + root + "/" + file)
+              root = re.escape(root)
+              file = re.escape(file)
+              # print(root + "/" + file)
+              os.system("clang-format -i -style=google " + root + "/" + file)
 
   print("Check issues related to code style ...")
   for root, dirs, files in os.walk("modules/frontend"):
       for file in files:
           if file.endswith(cpp_extensions):
               #print()
+              root = re.escape(root)
+              file = re.escape(file)
               os.system("python utils/cpplint.py --headers=hpp,h --linelength=120 --counting=detailed " + root + "/" + file )
 
   exit(0)
-
-
-
-#if [ "$1" == "CPP" ]; then
-#  python
-#else
-#  clang-format -i -style=.clang-format-llvm src/locuspocus.c
-#fi

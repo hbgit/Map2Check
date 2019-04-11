9ec3d00919285b3b017ec72b1cb3d16ced4f735a
Herbert Rocha <herberthb12@gmail.com>
2019-04-11 15:55:06 -0400

    Minor changes related on libstdc++.so.6
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- make-release.sh
+++ make-release.sh
@@ -48,7 +48,7 @@ cp /usr/lib/x86_64-linux-gnu/libbfd-2.30-system.so ./lib
 cp /usr/lib/gcc/x86_64-linux-gnu/5/libstdc++.a ./lib/
 cp /lib/x86_64-linux-gnu/libpthread.so.0 ./lib/libpthread.so
 cp /lib/x86_64-linux-gnu/libdl.so.2 ./lib/libdl.so
-cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./lib/libstdc++.so
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.25 ./lib/libstdc++.so.6
 cp /lib/x86_64-linux-gnu/libm.so.6 ./lib/libm.so
 cp /lib/x86_64-linux-gnu/libgcc_s.so.1 ./lib/libgcc_s.so
 cp /usr/lib/gcc/x86_64-linux-gnu/5/libgcc.a ./lib/
@@ -78,8 +78,11 @@ if [ ! -d "./bin/crabllvm" ]; then
     mv ./bin/crab ./bin/crabllvm
 fi
 
+echo "> Crab-LLVM replacing PATH"
+sed -i '54s/\"PATH\"/\"CLANG_PATH\"/' ./bin/crabllvm/bin/crabllvm.py
 cp /usr/lib/x86_64-linux-gnu/libbfd-2.30-system.so ./bin/crabllvm/lib/
 cp /usr/lib/x86_64-linux-gnu/libgomp.so.1 ./bin/crabllvm/lib/
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.25 ./bin/crabllvm/lib/libstdc++.so.6
 cp ./lib/libz3.so ./bin/crabllvm/lib/
 
 ../utils/cp_utils_file.sh

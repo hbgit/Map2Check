68e0ef5adb1db7d0ba1c07c655059901e5629064
Herbert Rocha <herberthb12@gmail.com>
2019-11-03 20:24:19 -0400

    Minor changes
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

---
+++ generate_tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ------------------------
+# Set Terminal Font Colors
+# ------------------------
+
+bold=$(tput bold)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+defaultcolor=$(tput setaf default)
+
+YEAR=$(date +"%Y")
+MONTH=$(date +"%m")
+DAY=$(date +"%d")
+
+GIT_TAG=v7.3.1.$YEAR-$MONTH.$DAY
+echo "${bold}${green}Tag id =  $GIT_TAG"
+
+git fetch --tags
+
+echo "${bold}${green}Creating TAG ..."
+git tag ${GIT_TAG} -a -m "Deploying ${GIT_TAG}"
+git push origin develop --tags
+
+echo "\n\n"

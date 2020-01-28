bab1b3662614e6f42fd2f93fd25d5e7a87f6289d
Herbert Rocha <herberthb12@gmail.com>
2020-01-28 13:26:03 -0400

    Add SPDX id in all cpp e hpp files in frontend
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- modules/frontend/map2check.hpp
+++ modules/frontend/map2check.hpp
@@ -3,7 +3,7 @@
  * This file is part of the Map2Check tool, and is made available under
  * the terms of the GNU General Public License version 2.
  *
- * SPDX-License-Identifier: (GPL-2.0 AND Apache-2.0 AND NCSA AND MIT)
+ * SPDX-License-Identifier: (GPL-2.0 AND Apache-2.0 AND NCSA AND MIT AND BSL-1.0)
  **/
 
 #ifndef MODULES_FRONTEND_MAP2CHECK_HPP_

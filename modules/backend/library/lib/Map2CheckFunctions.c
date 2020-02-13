fd476eb39ba9156e3acfd8ce94ff3d15f8b08a5e
Herbert Rocha <herberthb12@gmail.com>
2020-02-13 10:57:13 -0400

    Add SPX id in backend/library/lib
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- modules/backend/library/lib/Map2CheckFunctions.c
+++ modules/backend/library/lib/Map2CheckFunctions.c
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2014 - 2020 Map2Check tool
+ * This file is part of the Map2Check tool, and is made available under
+ * the terms of the GNU General Public License version 2.
+ *
+ * SPDX-License-Identifier: (GPL-2.0)
+ **/
 
 #include "../header/Map2CheckFunctions.h"
 #include "../header/AnalysisMode.h"
